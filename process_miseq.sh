209f41ac9c2701fd2163812e5b052eee336758d2
Peter Briggs <peter.briggs@manchester.ac.uk>
2013-09-27 13:04:00 +0100

    process_miseq.sh: version 2.1.0, updated to automatically log run details and put QC reports onto webserver.

--- process_miseq.sh
+++ process_miseq.sh
@@ -5,7 +5,7 @@
 # Wrapper to manage data and run each stage of the
 # auto_process_illumina.sh script for MiSEQ data
 #
-echo $(basename $0) version 2.0.0
+echo $(basename $0) version 2.1.0
 # Get settings
 settings_file=$(dirname $0)/process_miseq_setup.sh
 if [ ! -f "$settings_file" ] ; then
@@ -14,8 +14,18 @@ if [ ! -f "$settings_file" ] ; then
 fi
 echo Acquiring settings from $settings_file
 . $settings_file
-echo "ARCHIVE_DIR : $ARCHIVE_DIR"
-echo "SEQ_DATA_LOG: $SEQ_DATA_LOG"
+echo "ARCHIVE_DIR       : $ARCHIVE_DIR"
+echo "SEQ_DATA_LOG      : $SEQ_DATA_LOG"
+echo "QC_WEB_SERVER     : $QC_WEB_SERVER"
+echo "QC_WEB_SERVER_USER: $QC_WEB_SERVER_USER"
+echo "QC_WEB_SERVER_DIR : $QC_WEB_SERVER_DIR"
+echo "QC_WEB_SERVER_URL : $QC_WEB_SERVER_URL"
+if [ -z "$ARCHIVE_DIR" ] || [ -z "$SEQ_DATA_LOG" ] || \
+    [ -z "$QC_WEB_SERVER" ] || [ -z "$QC_WEB_SERVER_USER" ] || \
+    [ -z "$QC_WEB_SERVER_DIR" ] || [ -z "$QC_WEB_SERVER_URL" ] ; then
+   echo ERROR some settings are blank, stopping
+   exit 1
+fi
 # Deal with command line
 if [ $# -eq 0 ] ; then
    echo "Usage: $0 DATA_DIR DESCRIPTION"
@@ -41,13 +51,19 @@ auto_process_illumina.sh setup miseq $LOCAL_DATA_DIR
 cd $ANALYSIS_DIR
 auto_process_illumina.sh make_fastqs
 auto_process_illumina.sh run_qc
-# Remove local copy of primary data
-##/bin/rm -rf $LOCAL_DATA_DIR
 # Copy results to archive area
 cd ..
 rsync_seq_data.py --mirror $ANALYSIS_DIR $ARCHIVE_DIR
 # Log the run
-DESCRIPTION="$(analyse_illumina_run.py --summary $ANALYSIS_DIR) $DESCRIPTION"
+DESCRIPTION="$DESCRIPTION $(analyse_illumina_run.py --summary $ANALYSIS_DIR)"
 log_seq_data.sh $SEQ_DATA_LOG $ARCHIVE_DIR/$(date +%Y)/miseq/$ANALYSIS_DIR "$DESCRIPTION"
+# Transfer qc reports to web server
+qc_web_server=${QC_WEB_SERVER_USER}@${QC_WEB_SERVER}
+echo Copying QC reports to web server: $qc_web_server
+ssh $qc_web_server mkdir ${QC_WEB_SERVER_DIR}/$(basename $DATA_DIR)
+find $ANALYSIS_DIR -name "qc_report.*.zip" -exec scp {} $qc_web_server:${QC_WEB_SERVER_DIR}/$(basename $DATA_DIR) \;
+# Summarise
+echo QC reports available at ${QC_WEB_SERVER_URL}/$(basename $DATA_DIR)
+grep $ANALYSIS_DIR $SEQ_DATA_LOG
 ##
 #
---
+++ process_miseq_setup.sh
@@ -0,0 +1,10 @@
+# Setup file for process_miseq.sh
+#
+# ARCHIVE_DIR: parent directory for holding copies of the
+# analysis directories produced by process_miseq.sh
+export ARCHIVE_DIR=/home/pjb/scratchpad/Analysis
+#
+# SEQ_DATA_LOG: logging file
+export SEQ_DATA_LOG=/home/pjb/scratchpad/SEQ_DATA.log
+##
+#
