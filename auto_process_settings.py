25410f451f9cd84a4d051dcb1acbd129cbd449c9
Peter Briggs <peter.briggs@manchester.ac.uk>
2013-10-08 12:24:49 +0100

    auto_process_settings.py: added placeholders for archive and web server info, and import for local settings in separate file.

--- auto_process_settings.py
+++ auto_process_settings.py
@@ -1,12 +1,30 @@
 # Settings for auto_process module
 #
-# Customise for local site
 import JobRunner
 from auto_process_utils import AttributeDict
 #
 # Define runners for specific jobs
 runners = AttributeDict(
-    default=JobRunner.SimpleJobRunner(),
     bcl2fastq=JobRunner.SimpleJobRunner(),
     qc=JobRunner.SimpleJobRunner(),
 )
+#
+# Information for archiving analyses
+archive = AttributeDict(
+    dirn=None,
+    log=None,
+)
+#
+# Information for uploading QC reports
+qc_web_server = AttributeDict(
+    user=None,
+    server=None,
+    webdir=None,
+    url=None,
+)
+#
+# Import site-specific settings from local version
+try:
+    from auto_process_settings_local import *
+except ImportError,ex:
+    print "Unable to import local settings: %s" % ex
