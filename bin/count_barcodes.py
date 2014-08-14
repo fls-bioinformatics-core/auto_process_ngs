7123435a3c44a6e9b5a611402f3a77e5cb810390
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-08-14 14:54:40 +0100

    count_barcodes.py: add a threshold cutoff option for reporting and improve how matching indexes sequences are reported.

--- bin/count_barcodes.py
+++ bin/count_barcodes.py
@@ -17,7 +17,7 @@ Fastq files, and reports the most numerous.
 
 """
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 import FASTQFile
 import IlluminaData
@@ -74,6 +74,12 @@ def nreads(counts):
     """
     return sum([counts[x] for x in counts])
 
+def percent_reads(n,nreads):
+    """Return n as a percentage of nreads
+
+    """
+    return float(n)/nreads*100.0
+
 def nmismatches(seq1,seq2,truncate=True):
     """Return the number of mismatches between seq1 and seq2
 
@@ -94,7 +100,8 @@ def nmismatches(seq1,seq2,truncate=True):
             raise ValueError("mismatches undefined for sequences of unequal length")
     return sum(c1 != c2 for c1,c2 in zip(seq1,seq2))
 
-def report(counts,nseqs=20,exclude_ns=False,fp=None):
+def report(counts,nseqs=20,exclude_ns=False,fp=None,
+           cutoff=None):
     """Print a summary of the most numerous barcode sequences
 
     Arguments:
@@ -102,10 +109,18 @@ def report(counts,nseqs=20,exclude_ns=False,fp=None):
       nseqs: number of barcode sequences to report
       exclude_ns: if True then only report sequences which
         don't include any bases flagged as 'N'
+      cutoff: threshold fraction of reads below which not
+        to report matches (default is to report everything)
       fp: specify output stream to write report to (default
         is to use stdout)
 
     """
+    # Deal with cutoff
+    if cutoff is not None:
+        if cutoff < 1.0:
+            cutoff = cutoff*100.0
+        fp.write("Cutoff: %5.2f%%\n" % cutoff) 
+    # Get sequences in frequency order
     seqs = ordered_seqs(counts)
     if exclude_ns:
         seqs = filter(lambda x: x if x.count('N') == 0 else None,seqs)
@@ -116,43 +131,71 @@ def report(counts,nseqs=20,exclude_ns=False,fp=None):
     fp.write("Top %d barcode sequences%s\n" % (nseqs,
                                                (" (no 'N's)" if exclude_ns else "")))
     for i,seq in enumerate(seqs[:nseqs]):
+        if cutoff is not None and percent_reads(counts[seq],n) < cutoff:
+            # Stop reporting
+            break
         fp.write("% 8d\t%s\t% 8d\t% 5.1f%%\n" % \
-                 (i+1,seq,counts[seq],float(counts[seq])/n*100.0))
+                 (i+1,seq,counts[seq],percent_reads(counts[seq],n)))
 
-def match_barcodes(counts,barcodes,nseqs=20,max_mismatches=3,fp=None):
+def match_barcodes(counts,samples,nseqs=20,fp=None,
+                   max_mismatches=3,cutoff=None):
     """Find best matches for a set of supplied barcodes
 
     Arguments:
       counts: 'counts' dictionary returned by 'count_barcodes'
         nseqs: number of barcode sequences to report
-      barcodes: dictionary with keys as barcodes and
+      samples: dictionary with keys as barcodes and
         corresponding values as sample names
       nseqs: number of top barcode sequences to try and match
       max_mismatches: maximum number of mismatches to allow
         when reporting matches (will be corrected to allow
         for differing barcode lengths)
+      cutoff: threshold fraction of reads below which not
+        to report matches (default is to report everything)
       fp: specify output stream to write report to (default
         is to use stdout)
 
     """
     if fp is None: fp = sys.stdout
-    fp.write("\nBest matches to supplied barcode sequences:\n")
+    fp.write("\nBest matches to supplied barcode sequences\n")
+    # Deal with cutoff
+    if cutoff is not None:
+        if cutoff < 1.0:
+            cutoff = cutoff*100.0
+        fp.write("Cutoff: %5.2f%%\n" % cutoff) 
+    # Get sequences in frequency order
     seqs = ordered_seqs(counts)[:nseqs]
-    for barcode in barcodes:
+    n = nreads(counts)
+    # Find best matches for each sample barcode
+    for barcode in samples:
         matches = {}
         for seq in seqs:
             matches[seq] = nmismatches(barcode,seq)
         # Sort into order of least to most mismatches
         ordered_matches = sorted(matches.keys(),
                                  cmp=lambda x,y: cmp(matches[x],matches[y]))
+        best_match = ordered_matches[0]
         # Maximum number of mismatches
-        n_mismatches = max_mismatches + abs(len(barcode) - len(seqs[0]))
+        n_mismatches = max(max_mismatches + abs(len(barcode) - len(best_match)),
+                           matches[best_match])
         # Report top matches i.e. lowest number of mismatches
-        line = ["%s:%s" % (barcodes[barcode],barcode)]
-        for seq in ordered_matches:
-            if matches[seq] <= n_mismatches:
-                line.append("%s (%d/%d)" % (seq,matches[seq],counts[seq]))
-        fp.write("%s\n" % '\t'.join(line))
+        fp.write("%s\t%15s\t%s\t[%d]\t% 10d\t% 5.1f%%\n" % 
+                 (samples[barcode],barcode,
+                  best_match,
+                  matches[best_match],
+                  counts[best_match],
+                  percent_reads(counts[best_match],n)))
+        for seq in ordered_matches[1:]:
+            if (cutoff is not None and percent_reads(counts[seq],n) < cutoff) or \
+                matches[seq] <= n_mismatches:
+                # Stop reporting
+                break
+            fp.write("%s\t%15s\t%s\t[%d]\t% 10d\t% 5.1f%%\n" % 
+                     ('','',
+                      seq,
+                      matches[seq],
+                      counts[seq],
+                      percent_reads(counts[seq],n)))
 
 def output(counts,filen):
     """Write all sequences to a file
@@ -201,6 +244,10 @@ if __name__ == '__main__':
                  help="maximum number of mismatches to use when report best matches against "
                  "barcodes from SAMPLE_SHEET (default is 3). Note that this is corrected "
                  "to allow for differences in sequence lengths.")
+    p.add_option('-t','--threshold',action='store',dest='cutoff',default=None,type='float',
+                 help="percentage or fraction of reads that a sequence must appear in "
+                 "to be reported; sequences appearing fewer times will not be reported "
+                 "(default is to report all barcodes).")
     options,args = p.parse_args()
     # Check arguments
     if not args:
@@ -223,7 +270,7 @@ if __name__ == '__main__':
             seq = line.split('\t')[1]
             count = int(line.split('\t')[2])
             counts[seq] = count
-        report(counts,nseqs=options.n,fp=fp)
+        report(counts,nseqs=options.n,cutoff=options.cutoff,fp=fp)
         # Match barcodes to index sequences in sample sheet
         if options.sample_sheet:
             barcodes = dict()
@@ -234,6 +281,7 @@ if __name__ == '__main__':
             match_barcodes(counts,barcodes,
                            nseqs=options.n,
                            max_mismatches=options.mismatches,
+                           cutoff=options.cutoff,
                            fp=fp)
     elif len(args) == 1 and os.path.isdir(args[0]):
         # Dealing with a bclToFastq output dir
@@ -269,7 +317,7 @@ if __name__ == '__main__':
             for fq in fastq_in_lane[lane]:
                 fp.write("Fastq: %s\n" % fq)
             counts = count_barcodes(fastq_in_lane[lane])
-            report(counts,nseqs=options.n,fp=fp)
+            report(counts,nseqs=options.n,cutoff=options.cutoff,fp=fp)
             if options.counts_file:
                 counts_file = "%s.lane%s%s" % (os.path.splitext(options.counts_file)[0],
                                                lane,
@@ -284,11 +332,12 @@ if __name__ == '__main__':
                 match_barcodes(counts,barcodes,
                                nseqs=options.n,
                                max_mismatches=options.mismatches,
+                               cutoff=options.cutoff,
                                fp=fp)
     else:
         # One or more fastq files
         counts = count_barcodes(args)
-        report(counts,nseqs=options.n,fp=fp)
+        report(counts,nseqs=options.n,cutoff=options.cutoff,fp=fp)
         if options.counts_file:
             output(counts,options.counts_file)
 
