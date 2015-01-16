4a9a8d156c2509c29bbb9c97a0615dae94fe8261
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-01-16 17:25:25 +0000

    test_envmod.py: further attempt to build meaningful unit tests for envmod.py.

--- auto_process_ngs/test/test_envmod.py
+++ auto_process_ngs/test/test_envmod.py
@@ -8,31 +8,54 @@ import os
 import shutil
 import auto_process_ngs.envmod as envmod
 
-class TestEnvMod(unittest.TestCase):
+def _setup_example_modulefiles(*apps):
+    # Create set of fake modulefiles
+    modulesdir = tempfile.mkdtemp()
+    try:
+        os.environ['MODULEPATH'] += ":%s" % modulesdir
+        for mod in apps:
+            filen = os.path.join(modulesdir,mod)
+            open(filen,'w').write('#%%Module1.0\nprepend-path PATH /opt/%s\n' % mod)
+    except KeyError:
+        shutil.rmtree(modulesdir)
+        raise Exception("No MODULEPATH environment variable")
+    # Wipe existing environment
+    try:
+        os.environ["LOADEDMODULES"] = ''
+    except KeyError:
+        pass
+    return modulesdir
+
+def _teardown_example_modulefiles(modulesdir):
+    # Remove the temporary test directory
+    shutil.rmtree(modulesdir)
+
+class TestEnvModLoaded(unittest.TestCase):
     """Tests for the envmod module
 
     """
 
     def setUp(self):
-        # Create set of fake modulefiles
-        self.modulesdir = tempfile.mkdtemp()
-        try:
-            os.environ['MODULEPATH'] += ":%s" % self.modulesdir
-            for mod in ('app1','app2'):
-                filen = os.path.join(self.modulesdir,mod)
-                open(filen,'w').write('#%%Module1.0\nprepend-path PATH /opt/%s\n' % mod)
-        except KeyError:
-            shutil.rmtree(self.wd)
-            self.fail("Unable to set up test environment")
+        self.modulesdir = _setup_example_modulefiles('app1','app2')
 
     def tearDown(self):
-        # Remove the temporary test directory
-        shutil.rmtree(self.wd)
+        _teardown_example_modulefiles(self.modulesdir)
 
     def test_loaded(self):
         """envmod.loaded function returns list of loaded modules
         """
         self.assertEqual(envmod.loaded(),[])
+
+class TestEnvModLoad(unittest.TestCase):
+    """Tests for the envmod module
+
+    """
+
+    def setUp(self):
+        self.modulesdir = _setup_example_modulefiles('app1','app2')
+
+    def tearDown(self):
+        _teardown_example_modulefiles(self.modulesdir)
 
     def test_load(self):
         """envmod.load function loads specified module
@@ -47,13 +70,39 @@ class TestEnvMod(unittest.TestCase):
         """
         self.assertRaises(Exception,envmod.load,'app3')
 
+class TestEnvModUnload(unittest.TestCase):
+    """Tests for the envmod module
+
+    """
+
+    def setUp(self):
+        self.modulesdir = _setup_example_modulefiles('app1','app2')
+
+    def tearDown(self):
+        _teardown_example_modulefiles(self.modulesdir)
+
     def test_unload(self):
         """envmod.unload function unloads specified module
         """
         envmod.load('app1','app2')
-        self.assertEqual(envmod.loaded(),['app1','app2'])
+        loaded = envmod.loaded()
+        loaded.sort()
+        self.assertEqual(loaded,['app1','app2'])
         envmod.unload('app1')
-        self.assertEqual(envmod.loaded(),['app2'])
+        loaded = envmod.loaded()
+        loaded.sort()
+        self.assertEqual(loaded,['app2'])
+
+class TestEnvModUnloadAll(unittest.TestCase):
+    """Tests for the envmod module
+
+    """
+
+    def setUp(self):
+        self.modulesdir = _setup_example_modulefiles('app1','app2')
+
+    def tearDown(self):
+        _teardown_example_modulefiles(self.modulesdir)
 
     def test_unload_all(self):
         """envmod.unload_all function unloads all modules
