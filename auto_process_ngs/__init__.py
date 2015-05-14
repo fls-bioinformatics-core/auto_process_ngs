17adddf6dc383e4289876d9457767382bac3275e
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-05-14 14:40:17 +0100

    Bump version to 0.2.11.

--- auto_process_ngs/__init__.py
+++ auto_process_ngs/__init__.py
@@ -1,5 +1,5 @@
 # Current version of the library
-__version__ = '0.2.10'
+__version__ = '0.2.11'
 
 def get_version():
     """Returns a string with the current version of the library (e.g., "0.2.0")
