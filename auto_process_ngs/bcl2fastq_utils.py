ab34806485a7178ff6b17854b0de9d1d7f172d52
Peter Briggs <peter.briggs@manchester.ac.uk>
2016-01-07 09:02:57 +0000

    bcl2fastq_utils.py: add function to run bcl2fastq 2.17.* (issue #9).

--- auto_process_ngs/bcl2fastq_utils.py
+++ auto_process_ngs/bcl2fastq_utils.py
@@ -17,7 +17,8 @@ Utility functions for bcl to fastq conversion operations:
 make_custom_sample_sheet: create a fixed copy of a sample sheet file
 get_nmismatches: determine number of mismatches from bases mask
 get_bases_mask: get a bases mask string
-run_bcl2fastq_18: run bcl-to-fastq conversion from CASAVA/bcl2fastq 1.8.*
+run_bcl2fastq_1_8: run bcl-to-fastq conversion from CASAVA/bcl2fastq 1.8.*
+run_bcl2fastq_2_17: run bcl-to-fastq conversion from bcl2fastq 2.17.*
 
 """
 
@@ -239,4 +240,76 @@ def run_bcl2fastq_1_8(basecalls_dir,sample_sheet,
     # Check returncode
     if returncode != 0:
         logging.error("make returned %s" % returncode)
+    return returncode
+
+def run_bcl2fastq_2_17(basecalls_dir,sample_sheet,
+                       output_dir="Unaligned",
+                       mismatches=None,
+                       bases_mask=None,
+                       nprocessors=None,
+                       force=False,
+                       ignore_missing_bcl=False,
+                       no_lane_splitting=False):
+    """
+    Wrapper for running bcl2fastq 2.17.*
+
+    Runs the bcl2fastq 2.17.* software to generate fastq files
+       from bcl files.
+
+    Arguments:
+      basecalls_dir: path to the top-level directory holding the bcl
+        files (typically 'Data/Intensities/Basecalls/' subdirectory)
+      sample_sheet: path to the sample sheet file to use
+      output_dir: optional, path to the output directory. Defaults to
+        'Unaligned'. If this directory already exists then the
+        conversion will fail unless the force option is set to True
+      mismatches: optional, specify maximum number of mismatched bases
+        allowed for matching index sequences during multiplexing.
+        Recommended values are zero for indexes shorter than 6 base
+        pairs, 1 for indexes of 6 or longer
+        (If not specified and bases_mask is supplied then mismatches
+        will be derived automatically from the bases mask string)
+      bases_mask: optional, specify string indicating how to treat
+        each cycle within each read e.g. 'y101,I6,y101'
+      nprocessors: optional, number of processors to use when running
+        'make' step
+      force: optional, if True then force overwrite of an existing
+        output directory (default is False).
+      ignore_missing_bcl: optional, if True then interpret missing bcl
+        files as no call (default is False)
+      no_lane_splitting: optional, if True then output FASTQ files
+        will not be split by lane (default is False, i.e. do split
+        FASTQs by lane)
+
+    Returns:
+      0 on success; if a problem is encountered then returns -1 for
+      errors within the function (e.g. missing Makefile) or the exit
+      code from the failed program.
+
+    """
+    # Set up and run bcl2fastq2
+    bcl2fastq2_cmd = applications.bcl2fastq.bcl2fastq2(
+        basecalls_dir,
+        sample_sheet,
+        output_dir=output_dir,
+        mismatches=mismatches,
+        bases_mask=bases_mask,
+        force=force,
+        ignore_missing_bcl=ignore_missing_bcl,
+        no_lane_splitting=no_lane_splitting
+    )
+    # Check the executable exists
+    if not bcl2fastq2_cmd.has_exe:
+        logging.error("'%s' missing, cannot run" % bcl2fastq2_cmd.command)
+        return -1
+    print "Running command: %s" % bcl2fastq2_cmd
+    returncode = bcl2fastq2_cmd.run_subprocess()
+    # Check returncode
+    if returncode != 0:
+        logging.error("bcl2fastq returned %s" % returncode)
+        return returncode
+    # Check outputs (directory and makefile)
+    if not os.path.isdir(output_dir):
+        logging.error("Output directory '%s' not found" % output_dir)
+        return -1
     return returncode
