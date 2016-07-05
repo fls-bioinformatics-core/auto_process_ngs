5a74d6335841a1d58a062625a646ce07e913ac52
Peter Briggs <peter.briggs@manchester.ac.uk>
2016-07-05 11:43:58 +0100

    assign_barcodes.py: now obtains its functionality from the fastq_utils module.

--- bin/assign_barcodes.py
+++ bin/assign_barcodes.py
@@ -12,98 +12,7 @@ headers.
 
 """
 import optparse
-import gzip
-from bcftbx.FASTQFile import FastqIterator
-
-def assign_barcodes_single_end(fastq_in,fastq_out,n=5):
-    """
-    """
-    if fastq_out.endswith('.gz'):
-        fp = gzip.GzipFile(filename=fastq_out,mode='wb')
-    else:
-        fp = open(fastq_out,'w')
-    print "Processing reads from %s" % fastq_in
-    nread = 0
-    for read in FastqIterator(fastq_in):
-        # Extract new barcode sequence
-        barcode = read.sequence[:n]
-        # Truncate sequence and quality accordingly
-        sequence = read.sequence[n:]
-        quality = read.quality[n:]
-        # Assign new values and write to output
-        read.seqid.index_sequence = barcode
-        read.sequence = sequence
-        read.quality = quality
-        fp.write("%s\n" % read)
-        nread += 1
-    print "Finished (%d reads processed)" % nread
-
-fastq_r1 = """@MISEQ:34:000000000-A7PHP:1:1101:12552:1774 1:N:0:TAAGGCGA
-TTTACAACTAGCTTCTCTTTTTCTT
-+
->AA?131@C1FCGGGG1BFFGF1F3
-@MISEQ:34:000000000-A7PHP:1:1101:16449:1793 1:N:0:TAAGGCGA
-TCCCCAGTCTCAGCCCTACTCCACT
-+
-11>>>11CDFFFBCGGG1AFE11AB
-@MISEQ:34:000000000-A7PHP:1:1101:15171:1796 1:N:0:GCCTTACC
-CCACCACGCCTGGCTAATTTTTTTT
-+
-1>1>>AAAAAFAGGGGBFGGGGGG0
-@MISEQ:34:000000000-A7PHP:1:1101:18777:1797 1:N:0:TAAGNNNA
-CAGCAATATACACTTCACTCTGCAT
-+
-111>>1FBFFFFGGGGCBGEG3A3D
-@MISEQ:34:000000000-A7PHP:1:1101:18622:1812 1:N:0:TAAGGCGA
-GATAAAGACAGAGTCTTAATTAAAC
-+
-11>1>11DFFCFFDGGGB3BF313A
-"""
-
-fastq_r1_out = """@MISEQ:34:000000000-A7PHP:1:1101:12552:1774 1:N:0:TTTAC
-AACTAGCTTCTCTTTTTCTT
-+
-31@C1FCGGGG1BFFGF1F3
-@MISEQ:34:000000000-A7PHP:1:1101:16449:1793 1:N:0:TCCCC
-AGTCTCAGCCCTACTCCACT
-+
-11CDFFFBCGGG1AFE11AB
-@MISEQ:34:000000000-A7PHP:1:1101:15171:1796 1:N:0:CCACC
-ACGCCTGGCTAATTTTTTTT
-+
-AAAAAFAGGGGBFGGGGGG0
-@MISEQ:34:000000000-A7PHP:1:1101:18777:1797 1:N:0:CAGCA
-ATATACACTTCACTCTGCAT
-+
-1FBFFFFGGGGCBGEG3A3D
-@MISEQ:34:000000000-A7PHP:1:1101:18622:1812 1:N:0:GATAA
-AGACAGAGTCTTAATTAAAC
-+
-11DFFCFFDGGGB3BF313A
-"""
-
-import unittest
-import os
-import tempfile
-import shutil
-class TestAssignBarcodesSingleEnd(unittest.TestCase):
-    def setUp(self):
-        # Temporary working dir
-        self.wd = tempfile.mkdtemp(suffix='.test_split_single_end')
-        # Test file
-        self.fastq_in = os.path.join(self.wd,'test.fq')
-        open(self.fastq_in,'w').write(fastq_r1)
-        # Output file
-        self.fastq_out = os.path.join(self.wd,'out.fq')
-    def tearDown(self):
-        # Remove temporary working dir
-        if os.path.isdir(self.wd):
-            shutil.rmtree(self.wd)
-    def test_assign_barcodes_single_end(self):
-        assign_barcodes_single_end(self.fastq_in,
-                                   self.fastq_out)
-        self.assertEqual(open(self.fastq_out,'r').read(),
-                         fastq_r1_out)
+from auto_process_ngs.fastq_utils import assign_barcodes_single_end
 
 if __name__ == '__main__':
     p = optparse.OptionParser(usage="%prog [OPTIONS] INPUT.fq OUTPUT.fq",
@@ -119,7 +28,3 @@ if __name__ == '__main__':
     if len(args) != 2:
         p.error('Need to specify input and output files')
     assign_barcodes_single_end(args[0],args[1],opts.n)
-
-
-        
-        
