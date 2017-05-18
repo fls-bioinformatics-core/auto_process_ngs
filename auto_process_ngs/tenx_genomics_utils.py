66a47539081977f710ceb721947da0453ff6b1e6
Peter Briggs <peter.briggs@manchester.ac.uk>
2017-05-18 15:40:43 +0100

    process_10xgenomics.py: first version of a utility for 10xGenomics data.

---
+++ auto_process_ngs/tenx_genomics_utils.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+#
+#     tenx_genomics_utils.py: utility functions for handling 10xGenomics data
+#     Copyright (C) University of Manchester 2017 Peter Briggs
+#
+
+"""
+tenx_genomics_utils.py
+
+Utility classes and functions for processing the outputs from 10xGenomics's
+Chromium SC 3'v2 system:
+
+- make_qc_summary_html
+"""
+
+#######################################################################
+# Imports
+#######################################################################
+
+import json
+from .docwriter import Document
+from .docwriter import List
+from .docwriter import Link
+from .docwriter import Table
+import css_rules
+
+#######################################################################
+# Functions
+#######################################################################
+
+def make_qc_summary_html(json_file,html_file):
+    """
+    Generate HTML report of processing stats from cellranger mkfastqs
+    """
+    # Load the JSON data
+    with open(json_file,'r') as fp:
+        data = json.load(fp)
+    # Initialise the HTML report
+    qc_summary = Document("mkfastq QC report")
+    qc_summary.add_css_rule(css_rules.QC_REPORT_CSS_RULES)
+    qc_summary.add_css_rule("table { font-size: 80%;\n"
+                            "        font-family: sans-serif; }")
+    qc_summary.add_css_rule("td { text-align: right; }")
+    # Add table of contents
+    toc = qc_summary.add_section("Contents",name="toc")
+    toc_list = List()
+    toc.add(toc_list)
+    # General information
+    general_info = qc_summary.add_section("General information")
+    toc_list.add_item(Link("General information",general_info))
+    data_items = ['run_id',
+                  'experiment_name',
+                  '10x_software_version',
+                  'bcl2fastq_version',
+                  'bcl2fastq_args',
+                  'rta_version',]
+    tbl = Table(columns=['Parameter','Value'])
+    for item in data_items:
+        tbl.add_row(Parameter=item,Value=data[item])
+    general_info.add(tbl)
+    # Get the sample names
+    sample_names = data['sample_qc'].keys()
+    # Get names of the associated data items
+    sample0 = sample_names[0]
+    item_names = data['sample_qc'][sample0]['all'].keys()
+    # Report QC for each sample in tables
+    for sample in sample_names:
+        sample_qc = qc_summary.add_section("Sample: %s" % sample)
+        toc_list.add_item(Link("Sample: %s" % sample,sample_qc))
+        # Set up the table
+        tbl = Table(['items',],items="")
+        for item in item_names:
+            tbl.add_row(items=item)
+        # Lanes
+        lanes = data['sample_qc'][sample].keys()
+        for lane in lanes:
+            column = "%s" % lane 
+            tbl.append_columns(column)
+            # Add the data
+            for i,item in enumerate(item_names):
+                tbl.set_value(i,column,data['sample_qc'][sample][lane][item])
+        # Add to the document
+        sample_qc.add(tbl)
+    # Write the report
+    qc_summary.write(html_file)
+
+import sys
+if __name__ == "__main__":
+    json_file = sys.argv[1]
+    html_file = "qc_summary.html"
+    report_cellranger_mkfastqs_qc_summary(json_file,html_file)
---
+++ bin/process_10xgenomics.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python
+#
+#     process_10xgenomics.py: processing of 10xGenomics Chromium SC data
+#     Copyright (C) University of Manchester 2017 Peter Briggs
+#
+"""
+process_10genomics.py
+
+Utility to peform initial processing of data from 10xGenomics Chromium
+SC 3'v2 platform.
+"""
+
+######################################################################
+# Imports
+######################################################################
+
+import sys
+import os
+import argparse
+import logging
+from bcftbx.utils import mkdir
+from bcftbx.utils import find_program
+from bcftbx.IlluminaData import IlluminaData
+from bcftbx.IlluminaData import IlluminaDataError
+from bcftbx.JobRunner import SimpleJobRunner
+from auto_process_ngs.applications import Command
+from auto_process_ngs.simple_scheduler import SimpleScheduler
+from auto_process_ngs.simple_scheduler import SchedulerJob
+from auto_process_ngs.simple_scheduler import SchedulerReporter
+from auto_process_ngs.tenx_genomics_utils import make_qc_summary_html
+import auto_process_ngs.envmod as envmod
+
+# Initialise logging
+import logging
+logger = logging.getLogger(__name__)
+
+# Fetch configuration settings
+import auto_process_ngs.settings
+__settings = auto_process_ngs.settings.Settings()
+
+######################################################################
+# Functions
+######################################################################
+
+def cellranger_mkfastq(samplesheet,
+                       primary_data_dir,
+                       output_dir,
+                       lanes=None,
+                       cellranger_jobmode='sge',
+                       cellranger_maxjobs=None,
+                       cellranger_mempercore=None,
+                       cellranger_jobinterval=None,
+                       dry_run=False):
+    """
+    """
+    cmd = Command("cellranger","mkfastq"
+                  "--samplesheet",samplesheet,
+                  "--run",primary_data_dir,
+                  "--output-dir",output_dir)
+    if lanes is not None:
+        cmd.add_args("--lanes=%s" % lanes)
+    add_cellranger_args(cmd,
+                        jobmode=cellranger_jobmode,
+                        mempercore=cellranger_mempercore,
+                        maxjobs=cellranger_maxjobs,
+                        jobinterval=cellranger_jobinterval)
+    print "Running: %s" % cmd
+    if not dry_run:
+        cellranger_mkfastq_job = SchedulerJob(
+            SimpleJobRunner(),
+            cmd.command_line,
+            name='cellranger_mkfastq',
+            working_dir=os.getcwd())
+        cellranger_mkfastq_job.start()
+        try:
+            cellranger_mkfastq_job.wait()
+        except KeyboardInterrupt,ex:
+            logging.warning("Keyboard interrupt, terminating cellranger")
+            cellranger_mkfastq_job.terminate()
+            raise ex
+        exit_code = cellranger_mkfastq_job.exit_code
+        print "cellranger mkfastq completed: exit code %s" % exit_code
+        if exit_code != 0:
+            logging.error("cellranger mkfastq exited with an error")
+            return
+        # Deal with the QC summary report
+        flow_cell_dir = flow_cell_id(primary_data_dir)
+        if lanes is not None:
+            lanes_suffix = "_%s" % lanes.replace(',','')
+        else:
+            lanes_suffix = ""
+        flow_cell_dir = "%s%s" % (flow_cell_dir,lanes_suffix)
+        if not os.path.isdir(flow_cell_dir):
+            logging.error("No output directory '%s'" % flow_cell_dir)
+            return
+        json_file = os.path.join(flow_cell_dir,
+                                 "outs",
+                                 "qc_summary.json")
+        html_file = "qc_summary%s.html" % lanes_suffix
+        make_qc_summary_html(json_file,html_file)
+
+def cellranger_count(unaligned_dir,
+                     transcriptome,
+                     cellranger_jobmode='sge',
+                     cellranger_maxjobs=None,
+                     cellranger_mempercore=None,
+                     cellranger_jobinterval=None,
+                     max_jobs=4,
+                     dry_run=False):
+    """
+    """
+    # Input data
+    sample_names = {}
+    try:
+        illumina_data = IlluminaData(os.getcwd(),
+                                     unaligned_dir=unaligned_dir)
+        for project in illumina_data.projects:
+            sample_names[project.name] = []
+            for sample in project.samples:
+                sample_names[project.name].append(sample.name)
+    except IlluminaDataError:
+        logging.critical("Couldn't load data from '%s'" %
+                         unaligned_dir)
+        sys.exit(1)
+    print "Samples: %s" % sample_names
+
+    # Set up a scheduler
+    sched_reporter = SchedulerReporter(
+        job_start="SCHEDULER: Started  #%(job_number)d: %(job_name)s:\n-- %(command)s",
+        job_end=  "SCHEDULER: Finished #%(job_number)d: %(job_name)s"
+    )
+    sched_reporter = SchedulerReporter()
+    sched = SimpleScheduler(max_concurrent=max_jobs,
+                            reporter=sched_reporter
+    )
+    sched.start()
+
+    # Submit the cellranger count jobs
+    for project in sample_names:
+        print "Project: %s" % project
+        project_dir = os.path.abspath(project)
+        mkdir(project_dir)
+        log_dir = os.path.join(project_dir,"logs")
+        mkdir(log_dir)
+        for sample in sample_names[project]:
+            print "Sample: %s" % sample
+            name = "count_%s" % sample
+            cmd = Command("cellranger","count",
+                          "--id",name,
+                          "--fastqs",fastqs_dir,
+                          "--sample",sample,
+                          "--transcriptome",transcriptome)
+            add_cellranger_args(cmd,
+                                jobmode=cellranger_jobmode,
+                                mempercore=cellranger_mempercore,
+                                maxjobs=cellranger_maxjobs,
+                                jobinterval=cellranger_jobinterval)
+            print "Running: %s" % cmd
+            if not dry_run:
+                sched.submit(cmd,
+                             name="cellranger_count.%s.%s" %
+                             (project,
+                              sample),
+                             log_dir=log_dir)
+    sched.wait()
+
+def add_cellranger_args(cmd,
+                        jobmode='sge',
+                        maxjobs=None,
+                        mempercore=None,
+                        jobinterval=None):
+    """
+    """
+    if jobmode is not None:
+        cmd.add_args("--jobmode=%s" % jobmode)
+    if mempercore is not None:
+        cmd.add_args("--mempercore=%s" % mempercore)
+    if maxjobs is not None:
+        cmd.add_args("--maxjobs=%s" % maxjobs)
+    if jobinterval is not None:
+        cmd.add_args("--jobinterval=%s" % jobinterval)
+    return cmd
+
+def flow_cell_id(run_name):
+    """
+    """
+    flow_cell_id = os.path.basename(run_name).split("_")[-1]
+    return flow_cell_id[1:]
+
+######################################################################
+# Main
+######################################################################
+
+if __name__ == "__main__":
+    # Handle the command line
+    parser = argparse.ArgumentParser(
+        description="Perform operations on FASTQs from 10xGenomics "
+        "Chromium SC 3'v2 using 'cellranger'")
+    subparsers = parser.add_subparsers(dest='command',
+                                       help='Available commands')
+
+    # Build command-specific subparsers
+    # 'mkfastq' command
+    mkfastq_parser = subparsers.add_parser("mkfastq",
+                                           help="run 'cellranger mkfastq'")
+    mkfastq_parser.add_argument("-s","--samplesheet",
+                                dest="samplesheet",default=None,
+                                help="samplesheet file")
+    mkfastq_parser.add_argument("-r","--run",
+                                dest="run_dir",default=None,
+                                help="run directory")
+    mkfastq_parser.add_argument("-o","--output_dir",
+                                dest="output_dir",default=None,
+                                help="output directory")
+    mkfastq_parser.add_argument("-l","--lanes",
+                                dest="lanes",default=None,
+                                help="comma-separated list of lanes "
+                                "(optional)")
+    # 'count' parser
+    count_parser = subparsers.add_parser("count",
+                                         help="run 'cellranger count'")
+    count_parser.add_argument("-u","--unaligned",
+                              dest="unaligned_dir",default="bcl2fastq",
+                              help="'unaligned' dir with output from "
+                              "bcl2fastq")
+    count_parser.add_argument("-t","--transcriptome",
+                              dest="transcriptome",default=None,
+                              help="directory with reference data for "
+                              "transcriptome of interest")
+    # Add generic options
+    for p in (mkfastq_parser,count_parser):
+        p.add_argument("--jobmode",
+                       dest="job_mode",default="sge",
+                       help="job mode to run cellranger in (default: "
+                       "'sge'")
+        p.add_argument("--mempercore",
+                       dest="mem_per_core",default=5,
+                       help="memory assumed per core (in Gbs; "
+                       "default: 5Gb")
+        p.add_argument("--maxjobs",type=int,
+                       dest="max_jobs",
+                       default= __settings.general.max_concurrent_jobs,
+                       help="maxiumum number of concurrent jobs to run "
+                       "(default: %d)"
+                       % __settings.general.max_concurrent_jobs)
+        p.add_argument("--jobinterval",type=int,
+                       dest="job_interval",default=100,
+                       help="how often jobs are submitted (in ms; "
+                       "default: 100ms)")
+        p.add_argument('--modulefiles',action='store',
+                       dest='modulefiles',default=None,
+                       help="comma-separated list of environment "
+                       "modules to load before executing commands "
+                       "(overrides any modules specified in the global "
+                       "settings)")
+        p.add_argument('--dry-run',action='store_true',
+                       dest='dry_run',
+                       help="report commands that would be run "
+                       "(don't execute them)")
+    args = parser.parse_args()
+    print "command: %s" % args.command
+
+    # Deal with module files
+    if args.modulefiles is not None:
+        modulefiles = args.modulefiles.split(',')
+        for modulefile in modulefiles:
+            envmod.load(modulefile)
+
+    # Check for underlying programs
+    required = ["cellranger"]
+    for prog in required:
+        if find_program(prog) is None:
+            logging.critical("couldn't find '%s'" % prog)
+            sys.exit(1)
+
+    # Run the requested command
+    if args.command == "mkfastq":
+        cellranger_mkfastq(args.samplesheet,
+                           args.run_dir,
+                           args.output_dir,
+                           lanes=args.lanes,
+                           cellranger_jobmode=args.job_mode,
+                           cellranger_maxjobs=args.max_jobs,
+                           cellranger_mempercore=args.mem_per_core,
+                           cellranger_jobinterval=args.job_interval,
+                           dry_run=args.dry_run)
+    elif args.command == "count":
+        # Run cellranger count over the samples
+        cellranger_count(args.unaligned_dir,
+                         args.transcriptome,
+                         cellranger_jobmode=args.job_mode,
+                         cellranger_maxjobs=args.max_jobs,
+                         cellranger_mempercore=args.mem_per_core,
+                         cellranger_jobinterval=args.job_interval,
+                         max_jobs=4,
+                         dry_run=args.dry_run)
