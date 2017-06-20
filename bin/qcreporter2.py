5234346437e97f1a3a49053e80d94dd8d94d2e1f
Peter Briggs <peter.briggs@manchester.ac.uk>
2017-06-20 13:08:14 +0100

    run_qc/qcreporter2: allow alternate QC output dirs and report names.

--- bin/qcreporter2.py
+++ bin/qcreporter2.py
@@ -30,8 +30,18 @@ def main():
                               version="%prog "+get_version(),
                               description="Generate QC report for each directory "
                               "DIR")
+    p.add_option('--qc_dir',action='store',dest='qc_dir',default='qc',
+                 help="explicitly specify QC output directory (nb if "
+                 "supplied then the same QC_DIR will be used for each "
+                 "DIR. Non-absolute paths are assumed to be relative to "
+                 "DIR). Default: 'qc'")
+    p.add_option('-f','--filename',action='store',dest='filename',
+                 default=None,
+                 help="file name for output QC report (default: "
+                 "<DIR>/<QC_DIR>_report.html)")
     p.add_option('--verify',action='store_true',dest='verify',
-                 help="verify the QC products only (don't write the report)")
+                 help="verify the QC products only (don't write the "
+                 "report)")
     opts,args = p.parse_args()
     if len(args) < 1:
         p.error("Need to supply at least one directory")
@@ -42,15 +52,31 @@ def main():
         dir_path = os.path.abspath(d)
         p = AnalysisProject(project_name,dir_path)
         print "Project: %s" % p.name
+        if opts.qc_dir is None:
+            qc_dir = p.qc_dir
+        else:
+            qc_dir = opts.qc_dir
+        if not os.path.isabs(qc_dir):
+            qc_dir = os.path.join(p.dirn,qc_dir)
+        print "QC output dir: %s" % qc_dir
         print "-"*(len('Project: ')+len(p.name))
         print "%d samples | %d fastqs" % (len(p.samples),len(p.fastqs))
         if opts.verify:
-            if not QCReporter(p).verify():
+            if not QCReporter(p).verify(qc_dir=qc_dir):
                 print "Verification: FAILED"
             else:
                 print "Verification: OK"
         else:
-            qc = QCReporter(p).report()
+            qc_base = os.path.basename(qc_dir)
+            if opts.filename is None:
+                out_file = '%s_report.html' % qc_base
+            else:
+                out_file = opts.filename
+            if not os.path.isabs(out_file):
+                out_file = os.path.join(p.dirn,out_file)
+            print "Writing QC report to %s" % out_file
+            qc = QCReporter(p).report(qc_dir=qc_dir,
+                                      filename=out_file)
 
 if __name__ == '__main__':
     main()
