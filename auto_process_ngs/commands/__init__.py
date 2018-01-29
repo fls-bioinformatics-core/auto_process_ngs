f5576d1a5a74d7a87caaba7c4bb60db27818c5a5
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-01-29 14:05:41 +0000

    Move the 'setup_analysis_dirs' command into its own library module (with tests).

--- auto_process_ngs/commands/__init__.py
+++ auto_process_ngs/commands/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 #
 #     commands/__init__.py: core functions for autoprocess commands
-#     Copyright (C) University of Manchester 2017 Peter Briggs
+#     Copyright (C) University of Manchester 2017-2018 Peter Briggs
 #
 #######################################################################
 # Imports
 #######################################################################
 
+from setup_analysis_dirs_cmd import setup_analysis_dirs
 from publish_qc_cmd import publish_qc
 from archive_cmd import archive
