fe3342ca6786000e32a4080ccb02149bc54b7588
Peter Briggs <peter.briggs@manchester.ac.uk>
2019-06-18 12:05:24 +0100

    mock: update the 'MockCellrangerExe' class to handle 'cellranger[-atac] count'.

--- auto_process_ngs/mock.py
+++ auto_process_ngs/mock.py
@@ -1255,6 +1255,8 @@ cellranger%s (%s)
 Copyright (c) 2018 10x Genomics, Inc.  All rights reserved.
 -------------------------------------------------------------------------------
 """ % (self._path,cmd,self._version)
+        # Get executable name
+        cellranger_exe = os.path.basename(self._path)
         # Handle version request or no args
         print header
         if cmd == " --version" or not cmd:
@@ -1278,6 +1280,22 @@ Copyright (c) 2018 10x Genomics, Inc.  All rights reserved.
         mkfastq.add_argument("--maxjobs",action="store")
         mkfastq.add_argument("--jobinterval",action="store")
         mkfastq.add_argument("--disable-ui",action="store_true")
+        # count subparser
+        count = sp.add_parser("count")
+        count.add_argument("--id",action="store")
+        count.add_argument("--fastqs",action="store")
+        count.add_argument("--sample",action="store")
+        if cellranger_exe == "cellranger":
+            count.add_argument("--transcriptome",action="store")
+            count.add_argument("--chemistry",action="store")
+        elif cellranger_exe == "cellranger-atac":
+            count.add_argument("--reference",action="store")
+        count.add_argument("--jobmode",action="store")
+        count.add_argument("--localcores",action="store")
+        count.add_argument("--localmem",action="store")
+        count.add_argument("--mempercore",action="store")
+        count.add_argument("--maxjobs",action="store")
+        count.add_argument("--jobinterval",action="store")
         # Process command line
         args = p.parse_args()
         if args.command == "mkfastq":
@@ -1353,6 +1371,25 @@ Copyright (c) 2018 10x Genomics, Inc.  All rights reserved.
                 json_file = os.path.join(outs_dir,"qc_summary.json")
                 with open(json_file,'w') as fp:
                     fp.write(mock10xdata.QC_SUMMARY_JSON)
+        elif args.command == "count":
+            ###############
+            # count command
+            ###############
+            top_dir = str(args.id)
+            os.mkdir(top_dir)
+            outs_dir = os.path.join(top_dir,"outs")
+            os.mkdir(outs_dir)
+            if cellranger_exe == "cellranger":
+                metrics_file = os.path.join(outs_dir,"metrics_summary.csv")
+                with open(metrics_file,'w') as fp:
+                    fp.write(mock10xdata.METRICS_SUMMARY)
+            elif cellranger_exe == "cellranger-atac":
+                summary_file = os.path.join(outs_dir,"summary.csv")
+                with open(summary_file,'w') as fp:
+                    fp.write(mock10xdata.ATAC_SUMMARY)
+            web_summary_file = os.path.join(outs_dir,"web_summary.html")
+            with open(web_summary_file,'w') as fp:
+                fp.write("PLACEHOLDER FOR WEB_SUMMARY.HTML")
         else:
             print "%s: not implemented" % command
         print "Return exit code: %s" % self._exit_code
