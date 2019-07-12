97612e687a9bd1ced5409f13e7ceaa1a0abb39c2
Peter Briggs <peter.briggs@manchester.ac.uk>
2019-07-12 10:15:14 +0100

    download_fastqs.py: switch from optparse to argparse for command line parsing.

--- bin/download_fastqs.py
+++ bin/download_fastqs.py
@@ -18,7 +18,7 @@ Utility to download Fastq files from web server.
 # Imports
 #######################################################################
 
-import optparse
+import argparse
 import tempfile
 from urllib2 import urlopen
 import re
@@ -77,30 +77,32 @@ def check_md5sum(filen,md5):
 #######################################################################
 
 if __name__ == "__main__":
-    p = optparse.OptionParser(usage="%prog [OPTIONS] URL [DIR]",
-                              description="Download checksum file and fastqs from "
-                              "URL into current directory (or directory DIR, if "
-                              "specified), and verify the downloaded files against "
-                              "the checksum file.")
-    options,args = p.parse_args()
-    if not len(args):
-        p.error("Supply a URL to download Fastqs from, and an option target d")
-    elif len(args) > 2:
-        p.error("Too many arguments, use -h for usage information")
-    url = args[0]
-    if len(args) == 2:
-        dest_dir = os.path.abspath(args[1])
-        print "Moving to %s" % dest_dir
-        try:
-            os.chdir(dest_dir)
-        except Exception,ex:
-            if not os.path.isdir(dest_dir):
-                sys.stderr.write("ERROR directory doesn't exist?\n")
-            else:
-                sys.stderr.write("ERROR %s\n" % ex)
-            sys.exit(1)
-    print "Fetching index from %s" % url
-    index_page = urlopen(url).read()
+    p = argparse.ArgumentParser(
+        description="Download checksum file and fastqs from "
+        "URL into current directory (or directory DIR, if "
+        "specified), and verify the downloaded files against "
+        "the checksum file.")
+    p.add_argument('url',metavar="URL",
+                   help="URL with checksum file and fastqs")
+    p.add_argument('dir',metavar="DIR",
+                   nargs='?',help="directory to put downloaded "
+                   "fastqs into (defaults to current directory)")
+    args = p.parse_args()
+    if args.dir:
+        dest_dir = os.path.abspath(args.dir)
+    else:
+        dest_dir = os.getcwd()
+    print "Moving to %s" % dest_dir
+    try:
+        os.chdir(dest_dir)
+    except Exception,ex:
+        if not os.path.isdir(dest_dir):
+            sys.stderr.write("ERROR directory doesn't exist?\n")
+        else:
+            sys.stderr.write("ERROR %s\n" % ex)
+        sys.exit(1)
+    print "Fetching index from %s" % args.url
+    index_page = urlopen(args.url).read()
     print "Locating chksum file",
     for line in index_page.split('\n'):
         chksum_file = re.search('[A-Za-z0-9_]*\.chksums',line)
@@ -113,7 +115,7 @@ if __name__ == "__main__":
     chksum_file = chksum_file.group(0)
     # Download chksum file
     print "Fetching %s" % chksum_file
-    download_file(os.path.join(url,chksum_file),chksum_file)
+    download_file(os.path.join(args.url,chksum_file),chksum_file)
     # Get file list and checksums
     chksums = dict()
     with open(chksum_file,'r') as fp:
@@ -130,7 +132,7 @@ if __name__ == "__main__":
         if os.path.exists(f):
             print "%s: file exists, skipping download" % f
         else:
-            download_file(os.path.join(url,f),f)
+            download_file(os.path.join(args.url,f),f)
         if check_md5sum(f,chksums[f]):
             print "%s: checksum OK" % f
         else:
