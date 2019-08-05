5e3ae7724b55f2e4c9fe328a2037ca2d478a5458
Peter Briggs <peter.briggs@manchester.ac.uk>
2019-08-05 15:04:25 +0100

    update_project_metadata.py: switch from print function to print statement.

--- bin/update_project_metadata.py
+++ bin/update_project_metadata.py
@@ -44,11 +44,11 @@ if __name__ == "__main__":
     # Try and load the data
     try:
         run = AnalysisDir(args.dir)
-        print "Loaded data from %s" % args.dir
-        print "Found %d projects" % run.n_projects
+        print("Loaded data from %s" % args.dir)
+        print("Found %d projects" % run.n_projects)
         if run.undetermined is not None:
-            print "Found 'undetermined' analysis"
-        print "Found %d sequencing data directories" % run.n_sequencing_data
+            print("Found 'undetermined' analysis")
+        print("Found %d sequencing data directories" % run.n_sequencing_data)
     except Exception as ex:
         sys.stderr.write("Failed to load data for %s: %s\n" % (args.dir,ex))
         sys.exit(1)
@@ -56,22 +56,22 @@ if __name__ == "__main__":
     target_project = args.project
     projects = run.get_projects(pattern=target_project)
     if not projects:
-        print "No projects found"
+        print("No projects found")
         sys.exit(1)
-    print "%d projects selected" % len(projects)
+    print("%d projects selected" % len(projects))
     if (args.update or args.init) and len(projects) > 1:
         sys.stderr.write("-i/-u can't be used for multiple projects\n")
         sys.exit(1)
     # Check metadata for the projects
-    print "Checking metadata for projects"
+    print("Checking metadata for projects")
     for p in projects:
         # Check for metadata file
         has_metadata_file = os.path.exists(p.info_file)
         if not has_metadata_file:
             # No metadata file
-            print "%s: missing metadata file '%s'" % (p.name,p.info_file)
+            print("%s: missing metadata file '%s'" % (p.name,p.info_file))
             if args.init:
-                print "Initialising metadata file"
+                print("Initialising metadata file")
                 run_name = os.path.basename(os.path.abspath(args.dir))
                 if run_name.endswith('_analysis'):
                     run_name = run_name[:-9]
@@ -84,18 +84,21 @@ if __name__ == "__main__":
         else:
             # Check for metadata that is not assigned
             missing_data = []
-            for attr in ('run','user','PI','samples','library_type','organism','platform'):
+            for attr in ('run','user','PI','samples',
+                         'library_type','organism','platform'):
                 if p.info[attr] is None:
                     missing_data.append(attr)
             if missing_data:
-                print "%s: missing values for %s" % (p.name,', '.join(["'%s'" % x 
-                                                                       for x in missing_data]))
+                print("%s: missing values for %s" %
+                      (p.name,', '.join(["'%s'" %
+                                         x for x in missing_data])))
             else:
-                print "%s: ok" % p.name
+                print("%s: ok" % p.name)
         # Do updates
         if args.update:
             if not has_metadata_file:
-                sys.stderr.write("No metadata file: use -i/--init to create one first\n")
+                sys.stderr.write("No metadata file: use -i/--init to "
+                                 "create one first\n")
                 sys.exit(1)
             for pair in args.update:
                 try:
@@ -107,7 +110,7 @@ if __name__ == "__main__":
                 if key not in ('run','platform','user','PI','library_type','organism','comments'):
                     sys.stderr.write("Unable to update unrecognised item '%s'\n" % key)
                 else:
-                    print "Setting '%s' to '%s'" % (key,value)
+                    print("Setting '%s' to '%s'" % (key,value))
                     p.info[key] = value
             # Save the metadata
             dry_run = False
