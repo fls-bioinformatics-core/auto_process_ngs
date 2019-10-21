40c3f3835d1fce66750d72a85f1b1c4cf06bfcb4
Peter Briggs <peter.briggs@manchester.ac.uk>
2019-10-21 13:15:44 +0100

    commands/__init__: update to use relative imports.

--- auto_process_ngs/commands/__init__.py
+++ auto_process_ngs/commands/__init__.py
@@ -7,15 +7,15 @@
 # Imports
 #######################################################################
 
-from setup_cmd import setup
-from setup_analysis_dirs_cmd import setup_analysis_dirs
-from make_fastqs_cmd import make_fastqs
-from analyse_barcodes_cmd import analyse_barcodes
-from run_qc_cmd import run_qc
-from publish_qc_cmd import publish_qc
-from archive_cmd import archive
-from report_cmd import report
-from merge_fastq_dirs_cmd import merge_fastq_dirs
-from update_fastq_stats_cmd import update_fastq_stats
-from import_project_cmd import import_project
-from clone_cmd import clone
+from .setup_cmd import setup
+from .setup_analysis_dirs_cmd import setup_analysis_dirs
+from .make_fastqs_cmd import make_fastqs
+from .analyse_barcodes_cmd import analyse_barcodes
+from .run_qc_cmd import run_qc
+from .publish_qc_cmd import publish_qc
+from .archive_cmd import archive
+from .report_cmd import report
+from .merge_fastq_dirs_cmd import merge_fastq_dirs
+from .update_fastq_stats_cmd import update_fastq_stats
+from .import_project_cmd import import_project
+from .clone_cmd import clone
