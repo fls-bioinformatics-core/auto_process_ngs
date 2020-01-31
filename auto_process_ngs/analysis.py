da82675242caeb58611050016da03dfa4bf0631c
Peter Briggs <peter.briggs@manchester.ac.uk>
2020-01-31 13:42:35 +0000

    analysis: import 'reduce' from functools for Python3 compatibility.

--- auto_process_ngs/analysis.py
+++ auto_process_ngs/analysis.py
@@ -47,6 +47,7 @@ from .metadata import ProjectMetadataFile
 from .metadata import AnalysisProjectQCDirInfo
 from .fastq_utils import BaseFastqAttrs
 from .fastq_utils import IlluminaFastqAttrs
+from functools import reduce
 
 # Module specific logger
 logger = logging.getLogger(__name__)
