c4e9ce979d4f458684bb12a90564b196961d80b2
Peter Briggs <peter.briggs@manchester.ac.uk>
2020-03-23 13:16:47 +0000

    tenx_genomics_utils: add new unit test for 'set_cell_count_for_project' when project doesn't have library type defined.

--- auto_process_ngs/test/test_tenx_genomics.py
+++ auto_process_ngs/test/test_tenx_genomics.py
@@ -651,3 +651,34 @@ class TestSetCellCountForProject(unittest.TestCase):
         self.assertEqual(AnalysisProject("PJB1",
                                          project_dir).info.number_of_cells,
                          5682)
+    def test_set_cell_count_project_missing_library_type(self):
+        """
+        set_cell_count_for_project: test for scRNA-seq when library not set
+        """
+        # Set up mock project with library type not set
+        project_dir = self._make_mock_analysis_project(
+            "10xGenomics Chromium 3'v3",
+            None)
+        # Add metrics_summary.csv
+        counts_dir = os.path.join(project_dir,
+                                  "qc",
+                                  "cellranger_count",
+                                  "PJB1",
+                                  "outs")
+        mkdirs(counts_dir)
+        metrics_summary_file = os.path.join(counts_dir,
+                                            "metrics_summary.csv")
+        with open(metrics_summary_file,'w') as fp:
+            fp.write(METRICS_SUMMARY)
+        # Check initial cell count
+        print("Checking number of cells")
+        self.assertEqual(AnalysisProject("PJB1",
+                                         project_dir).info.number_of_cells,
+                         None)
+        # Update the cell counts
+        print("Updating number of cells")
+        set_cell_count_for_project(project_dir)
+        # Check updated cell count
+        self.assertEqual(AnalysisProject("PJB1",
+                                         project_dir).info.number_of_cells,
+                         2272)
