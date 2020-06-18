391092f605108668681e8235cce998bd343d4045
Peter Briggs <peter.briggs@manchester.ac.uk>
2020-06-18 10:52:17 +0100

    commands/update_fastq_stats: duplicate code from 'make_fastqs_cmd' and update tests, to make the stats update standalone.

--- auto_process_ngs/commands/update_fastq_stats_cmd.py
+++ auto_process_ngs/commands/update_fastq_stats_cmd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 #     update_fastq_stats_cmd.py: implement update_fastq_stats command
-#     Copyright (C) University of Manchester 2019 Peter Briggs
+#     Copyright (C) University of Manchester 2019-2020 Peter Briggs
 #
 #########################################################################
 
@@ -9,8 +9,12 @@
 # Imports
 #######################################################################
 
+import os
+from bcftbx import IlluminaData
+from ..applications import Command
+from ..bcl2fastq.reporting import ProcessingQCReport
+from ..simple_scheduler import SchedulerJob
 import logging
-from .make_fastqs_cmd import fastq_statistics
 
 # Module specific logger
 logger = logging.getLogger(__name__)
@@ -60,3 +64,192 @@ def update_fastq_stats(ap,stats_file=None,per_lane_stats_file=None,
                      force=force,
                      nprocessors=nprocessors,
                      runner=runner)
+
+def fastq_statistics(ap,stats_file=None,per_lane_stats_file=None,
+                     unaligned_dir=None,sample_sheet=None,add_data=False,
+                     force=False,nprocessors=None,runner=None):
+    """Generate statistics for Fastq files
+
+    Generates statistics for all Fastq files found in the
+    'unaligned' directory, by running the 'fastq_statistics.py'
+    program.
+
+    Arguments
+      ap (AutoProcessor): autoprocessor pointing to the analysis
+        directory to create Fastqs for
+      stats_file (str): path of a non-default file to write the
+        statistics to (defaults to 'statistics.info' unless
+        over-ridden by local settings)
+      per_lane_stats_file (str): path for per-lane statistics
+        output file (defaults to 'per_lane_statistics.info'
+        unless over-ridden by local settings)
+      unaligned_dir (str): output directory for bcl-to-fastq
+        conversion
+      sample_sheet (str): path to sample sheet file used in
+        bcl-to-fastq conversion
+      add_data (bool): if True then add stats to the existing
+        stats files (default is to overwrite existing stats
+        files)
+      force (bool): if True then force update of the stats
+        files even if they are newer than the Fastq files
+        (by default stats are only updated if they are older
+        than the Fastqs)
+      nprocessors (int): number of cores to use when running
+        'fastq_statistics.py'
+      runner (JobRunner): (optional) specify a non-default job
+        runner to use for running 'fastq_statistics.py'
+    """
+    # Get file names for output files
+    if stats_file is None:
+        if ap.params['stats_file'] is not None:
+            stats_file = ap.params['stats_file']
+        else:
+            stats_file='statistics.info'
+        if per_lane_stats_file is None:
+            if ap.params['per_lane_stats_file'] is not None:
+                per_lane_stats_file = ap.params['per_lane_stats_file']
+            else:
+                per_lane_stats_file='per_lane_statistics.info'
+    # Sort out unaligned_dir
+    if unaligned_dir is None:
+        if ap.params.unaligned_dir is None:
+            ap.params['unaligned_dir'] = 'bcl2fastq'
+        unaligned_dir = ap.params.unaligned_dir
+    if not os.path.exists(os.path.join(ap.params.analysis_dir,unaligned_dir)):
+        logger.error("Unaligned dir '%s' not found" % unaligned_dir)
+    # Check for sample sheet
+    if sample_sheet is None:
+        sample_sheet = ap.params['sample_sheet']
+    # Check if any Fastqs are newer than stats files
+    newest_mtime = 0
+    for f in (stats_file,per_lane_stats_file,):
+        try:
+            newest_mtime = max(newest_mtime,
+                               os.path.getmtime(f))
+        except OSError:
+            # Missing file
+            newest_mtime = 0
+            break
+    illumina_data = IlluminaData.IlluminaData(ap.params.analysis_dir,
+                                              unaligned_dir)
+    if newest_mtime > 0:
+        regenerate_stats = False
+        for project in illumina_data.projects:
+            for sample in project.samples:
+                for fq in sample.fastq:
+                    if (os.path.getmtime(os.path.join(sample.dirn,fq)) >
+                        newest_mtime):
+                        regenerate_stats = True
+                        break
+        if regenerate_stats:
+            logger.warning("Fastqs are newer than stats files")
+        else:
+            logger.warning("Stats files are newer than Fastqs")
+            if not force:
+                # Don't rerun the stats, just regenerate the report
+                processing_qc_html = os.path.join(ap.analysis_dir,
+                                                  "processing_qc.html")
+                report_processing_qc(ap,processing_qc_html)
+                return
+    # Set up runner
+    if runner is None:
+        runner = ap.settings.runners.stats
+    runner.set_log_dir(ap.log_dir)
+    # Number of cores
+    if nprocessors is None:
+        nprocessors = ap.settings.fastq_stats.nprocessors
+    # Generate statistics
+    fastq_statistics_cmd = Command(
+        'fastq_statistics.py',
+        '--unaligned',unaligned_dir,
+        '--sample-sheet',sample_sheet,
+        '--output',os.path.join(ap.params.analysis_dir,stats_file),
+        '--per-lane-stats',os.path.join(ap.params.analysis_dir,
+                                        per_lane_stats_file),
+        ap.params.analysis_dir,
+        '--nprocessors',nprocessors
+    )
+    if add_data:
+        fastq_statistics_cmd.add_args('--update')
+    print("Generating statistics: running %s" % fastq_statistics_cmd)
+    fastq_statistics_job = SchedulerJob(
+        runner,
+        fastq_statistics_cmd.command_line,
+        name='fastq_statistics',
+        working_dir=ap.analysis_dir
+    )
+    fastq_statistics_job.start()
+    try:
+        fastq_statistics_job.wait(
+            poll_interval=ap.settings.general.poll_interval
+        )
+    except KeyboardInterrupt as ex:
+        logger.warning("Keyboard interrupt, terminating fastq_statistics")
+        fastq_statistics_job.terminate()
+        raise ex
+    exit_code = fastq_statistics_job.exit_code
+    print("fastq_statistics completed: exit code %s" % exit_code)
+    if exit_code != 0:
+        raise Exception("fastq_statistics exited with an error")
+    ap.params['stats_file'] = stats_file
+    ap.params['per_lane_stats_file'] = per_lane_stats_file
+    print("Statistics generation completed: %s" % ap.params.stats_file)
+    print("Generating processing QC report")
+    processing_qc_html = os.path.join(ap.analysis_dir,
+                                      "processing_qc.html")
+    report_processing_qc(ap,processing_qc_html)
+
+def report_processing_qc(ap,html_file):
+    """
+    Generate HTML report for processing statistics
+
+    Arguments:
+      ap (AutoProcess): AutoProcess instance to report the
+        processing from
+      html_file (str): destination path and file name for
+        HTML report
+    """
+    # Per-lane statistics
+    per_lane_stats_file = ap.params.per_lane_stats_file
+    if per_lane_stats_file is None:
+        per_lane_stats_file = "per_lane_statistics.info"
+    per_lane_stats_file = get_absolute_file_path(per_lane_stats_file,
+                                                 base=ap.analysis_dir)
+    # Per lane by sample statistics
+    per_lane_sample_stats_file = get_absolute_file_path(
+        "per_lane_sample_stats.info",
+        base=ap.analysis_dir)
+    # Per fastq statistics
+    stats_file = get_absolute_file_path("statistics_full.info",
+                                        base=ap.analysis_dir)
+    if not os.path.exists(stats_file):
+        if ap.params.stats_file is not None:
+            stats_file = ap.params.stats_file
+        else:
+            stats_file = "statistics.info"
+    stats_file = get_absolute_file_path(stats_file,
+                                        base=ap.analysis_dir)
+    # Generate the report
+    ProcessingQCReport(ap.analysis_dir,
+                       stats_file,
+                       per_lane_stats_file,
+                       per_lane_sample_stats_file).write(html_file)
+
+def get_absolute_file_path(p,base=None):
+    """
+    Get absolute path for supplied path
+
+    Arguments:
+      p (str): path
+      base (str): optional, base directory to
+        use if p is relative
+
+    Returns:
+      String: absolute path for p.
+    """
+    if not os.path.isabs(p):
+        if base is not None:
+            p = os.path.join(os.path.abspath(base),p)
+        else:
+            p = os.path.abspath(p)
+    return p
--- auto_process_ngs/test/commands/test_update_fastq_stats_cmd.py
+++ auto_process_ngs/test/commands/test_update_fastq_stats_cmd.py
@@ -9,6 +9,7 @@ import os
 from auto_process_ngs.auto_processor import AutoProcess
 from auto_process_ngs.mock import MockAnalysisDirFactory
 from auto_process_ngs.commands.update_fastq_stats_cmd import update_fastq_stats
+from auto_process_ngs.commands.update_fastq_stats_cmd import report_processing_qc
 
 # Set to False to keep test output dirs
 REMOVE_TEST_OUTPUTS = True
@@ -66,3 +67,85 @@ class TestAutoProcessUpdateFastqStats(unittest.TestCase):
         for filen in stats_files:
             self.assertTrue(os.path.exists(os.path.join(mockdir.dirn,filen)),
                              "%s: missing" % filen)
+
+class TestReportProcessingQC(unittest.TestCase):
+    """
+    Tests for report_processing_qc function
+    """
+    def setUp(self):
+        # Create a temp working dir
+        self.wd = tempfile.mkdtemp(suffix='TestReportProcessingQC')
+
+    def tearDown(self):
+        if REMOVE_TEST_OUTPUTS:
+            shutil.rmtree(self.wd)
+
+    #@unittest.skip("Skipped")
+    def test_report_processing_qc(self):
+        """report_processing_qc: standard report
+        """
+        # Create test data
+        analysis_dir = os.path.join(self.wd,
+                                    "180430_K00311_0001_ABCDEFGHXX_analysis")
+        os.mkdir(analysis_dir)
+        per_lane_sample_stats = os.path.join(analysis_dir,
+                                             "per_lane_sample_stats.info")
+        with open(per_lane_sample_stats,'w') as fp:
+            fp.write("""
+Lane 1
+Total reads = 136778255
+- AB/AB1	25058003	18.3%
+- AB/AB2	22330927	16.3%
+- AB/AB3	34509382	25.2%
+- AB/AB4	27283286	19.9%
+- Undetermined_indices/undetermined	27596657	20.2%
+
+Lane 2
+Total reads = 136778255
+- CDE/CDE1	25058003	18.3%
+- CDE/CDE2	22330927	16.3%
+- CDE/CDE3	34509382	25.2%
+- CDE/CDE4	27283286	19.9%
+- Undetermined_indices/undetermined	27596657	20.2%
+""")
+        per_lane_statistics = os.path.join(analysis_dir,
+                                           "per_lane_statistics.info")
+        with open(per_lane_statistics,'w') as fp:
+            fp.write("""#Lane	Total reads	Assigned reads	Unassigned reads	%assigned	%unassigned
+Lane 1	136778255	109181598	27596657	79.8	20.2
+Lane 2	136778255	109181598	27596657	79.8	20.2
+""")
+        statistics_full = os.path.join(analysis_dir,
+                                       "statistics_full.info")
+        with open(statistics_full,'w') as fp:
+            fp.write("""#Project	Sample	Fastq	Size	Nreads	Paired_end	Read_number	L1	L2
+AB	AB1	AB1_S1_R1_001.fastq.gz	1.0G	25058003	Y	1	25058003	
+AB	AB1	AB1_S1_R2_001.fastq.gz	1.1G	25058003	Y	2	25058003	
+AB	AB2	AB2_S2_R1_001.fastq.gz	941.7M	22330927	Y	1	22330927	
+AB	AB2	AB2_S2_R2_001.fastq.gz	1.0G	22330927	Y	2	22330927	
+AB	AB3	AB3_S3_R1_001.fastq.gz	1.4G	34509382	Y	1	34509382	
+AB	AB3	AB3_S3_R2_001.fastq.gz	1.6G	34509382	Y	2	34509382	
+AB	AB4	AB4_S4_R1_001.fastq.gz	1.1G	27283286	Y	1	27283286	
+AB	AB4	AB4_S4_R2_001.fastq.gz	1.2G	27283286	Y	2	27283286	
+CDE	CDE1	CDE1_S5_R1_001.fastq.gz	1.0G	25058003	Y	1		25058003
+CDE	CDE1	CDE1_S5_R2_001.fastq.gz	1.1G	25058003	Y	2		25058003
+CDE	CDE2	CDE2_S6_R1_001.fastq.gz	941.7M	22330927	Y	1		22330927
+CDE	CDE2	CDE2_S6_R2_001.fastq.gz	1.0G	22330927	Y	2		22330927
+CDE	CDE3	CDE3_S7_R1_001.fastq.gz	1.4G	34509382	Y	1		34509382
+CDE	CDE3	CDE3_S7_R2_001.fastq.gz	1.6G	34509382	Y	2		34509382
+CDE	CDE4	CDE4_S8_R1_001.fastq.gz	1.1G	27283286	Y	1		27283286
+CDE	CDE4	CDE4_S8_R2_001.fastq.gz	1.2G	27283286	Y	2		27283286
+Undetermined_indices	undetermined	Undetermined_S0_R1_001.fastq.gz	22.0G	27596657	Y	1	27596657	27596657
+Undetermined_indices	undetermined	Undetermined_S0_R2_001.fastq.gz	24.0G	27596657	Y	2	27596657	27596657
+""")
+        # Generate QC report
+        output_html = os.path.join(analysis_dir,
+                                   "processing_report.html")
+        self.assertFalse(os.path.exists(output_html))
+        report_processing_qc(AutoProcess(analysis_dir),output_html)
+        self.assertTrue(os.path.exists(output_html))
+        # Check the HTML
+        with open(output_html,'rt') as fp:
+            html = fp.read()
+        # No warnings
+        self.assertTrue(html.find("<p>Status: OK</p>") > -1)
