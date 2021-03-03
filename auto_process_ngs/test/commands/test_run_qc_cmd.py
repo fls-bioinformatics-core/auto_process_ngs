2477fe308a9600e8813e342c8f70edeb53cdd18d
Peter Briggs <peter.briggs@manchester.ac.uk>
2021-03-03 17:51:56 +0000

    commands/run_qc: update unit tests to include cellranger 6.0.0 for single nuclei RNA-seq.

--- auto_process_ngs/test/commands/test_run_qc_cmd.py
+++ auto_process_ngs/test/commands/test_run_qc_cmd.py
@@ -600,6 +600,99 @@ mouse = /data/cellranger/transcriptomes/mm10
                     "multiqc_report.html")
                 self.assertTrue(multiqc in z.namelist())
 
+    def test_run_qc_10x_snRNAseq_600(self):
+        """run_qc: 10x snRNA-seq with strandedness and single library analysis (cellranger 6.0.0)
+        """
+        # Make mock illumina_qc.sh and multiqc
+        MockIlluminaQcSh.create(os.path.join(self.bin,
+                                             "illumina_qc.sh"))
+        MockFastqStrandPy.create(os.path.join(self.bin,
+                                              "fastq_strand.py"))
+        MockCellrangerExe.create(os.path.join(self.bin,"cellranger"),
+                                 version="6.0.0",
+                                 assert_include_introns=True)
+        MockMultiQC.create(os.path.join(self.bin,"multiqc"))
+        os.environ['PATH'] = "%s:%s" % (self.bin,
+                                        os.environ['PATH'])
+        # Make mock analysis directory
+        mockdir = MockAnalysisDirFactory.bcl2fastq2(
+            '170901_M00879_0087_000000000-AGEW9',
+            'miseq',
+            metadata={ "instrument_datestamp": "170901" },
+            project_metadata={ "AB": { "Organism": "human",
+                                       "Single cell platform":
+                                       "10xGenomics Chromium 3'v3",
+                                       "Library type": "snRNA-seq", },
+                               "CDE": { "Organism": "mouse",
+                                        "Single cell platform":
+                                        "10xGenomics Chromium 3'v3",
+                                        "Library type": "snRNA-seq", } },
+            top_dir=self.dirn)
+        mockdir.create()
+        # Settings file with fastq_strand indexes and
+        # polling interval
+        settings_ini = os.path.join(self.dirn,"auto_process.ini")
+        with open(settings_ini,'w') as s:
+            s.write("""[general]
+poll_interval = 1.0
+
+[fastq_strand_indexes]
+human = /data/genomeIndexes/hg38/STAR
+mouse = /data/genomeIndexes/mm10/STAR
+
+[10xgenomics_transcriptomes]
+human = /data/cellranger/transcriptomes/hg38
+mouse = /data/cellranger/transcriptomes/mm10
+""")
+        # Make autoprocess instance
+        ap = AutoProcess(analysis_dir=mockdir.dirn,
+                         settings=Settings(settings_ini))
+        # Run the QC
+        status = run_qc(ap,
+                        run_multiqc=True,
+                        max_jobs=1)
+        self.assertEqual(status,0)
+        # Check the fastq_strand_conf files were created
+        for p in ("AB","CDE"):
+            self.assertTrue(os.path.exists(
+                os.path.join(mockdir.dirn,p,"qc","fastq_strand.conf")))
+        # Check fastq_strand outputs are present
+        for p in ("AB","CDE"):
+            fastq_strand_outputs = list(
+                filter(lambda f:
+                       f.endswith("fastq_strand.txt"),
+                       os.listdir(os.path.join(
+                           mockdir.dirn,p,"qc"))))
+            self.assertTrue(len(fastq_strand_outputs) > 0)
+        # Check cellranger count outputs are present
+        for p in ("AB","CDE"):
+            self.assertTrue(os.path.exists(os.path.join(mockdir.dirn,
+                                                        p,
+                                                        "qc",
+                                                        "cellranger_count")))
+        # Check output and reports
+        for p in ("AB","CDE","undetermined"):
+            for f in ("qc",
+                      "qc_report.html",
+                      "qc_report.%s.%s.zip" % (
+                          p,
+                          '170901_M00879_0087_000000000-AGEW9'),
+                      "multiqc_report.html"):
+                self.assertTrue(os.path.exists(os.path.join(mockdir.dirn,
+                                                            p,f)),
+                                "Missing %s in project '%s'" % (f,p))
+            # Check zip file has MultiQC report
+            zip_file = os.path.join(mockdir.dirn,p,
+                                    "qc_report.%s.%s.zip" % (
+                                        p,
+                                        '170901_M00879_0087_000000000-AGEW9'))
+            with zipfile.ZipFile(zip_file) as z:
+                multiqc = os.path.join(
+                    "qc_report.%s.%s" % (
+                        p,'170901_M00879_0087_000000000-AGEW9'),
+                    "multiqc_report.html")
+                self.assertTrue(multiqc in z.namelist())
+
     def test_run_qc_10x_scATACseq(self):
         """run_qc: 10x scATAC-seq with strandedness and single library analysis
         """
