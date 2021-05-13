e7ba1549c2091de9ae366ca2c85f2f41ba0b1a48
Peter Briggs <peter.briggs@manchester.ac.uk>
2021-05-13 15:11:08 +0100

    bcl2fastq/pipeline: update tests for '10x_atac' protocol, to include cellranger-atac 2.0.0.

--- auto_process_ngs/test/bcl2fastq/test_pipeline.py
+++ auto_process_ngs/test/bcl2fastq/test_pipeline.py
@@ -3248,9 +3248,9 @@ smpl2,smpl2,,,A005,SI-GA-B1,10xGenomics,
                             "Missing file: %s" % filen)
 
     #@unittest.skip("Skipped")
-    def test_makefastqs_10x_atac_protocol(self):
+    def test_makefastqs_10x_atac_protocol_120(self):
         """
-        MakeFastqs: '10x_atac' protocol
+        MakeFastqs: '10x_atac' protocol (Cellranger-ATAC 1.2.0)
         """
         # Create mock source data
         illumina_run = MockIlluminaRun(
@@ -3285,7 +3285,8 @@ smpl2,smpl2,,,A005,SI-NA-B1,10xGenomics,
         MockBcl2fastq2Exe.create(os.path.join(self.bin,
                                               "bcl2fastq"))
         MockCellrangerExe.create(os.path.join(self.bin,
-                                              "cellranger-atac"))
+                                              "cellranger-atac"),
+                                 version="1.2.0")
         os.environ['PATH'] = "%s:%s" % (self.bin,
                                         os.environ['PATH'])
         analysis_dir = os.path.join(self.wd,"analysis")
@@ -3337,6 +3338,100 @@ smpl2,smpl2,,,A005,SI-NA-B1,10xGenomics,
                       "per_lane_sample_stats.info",
                       "processing_qc.html",
                       "cellranger-atac_qc_summary.html",):
+            self.assertTrue(os.path.isfile(
+                os.path.join(analysis_dir,filen)),
+                            "Missing file: %s" % filen)
+
+    #@unittest.skip("Skipped")
+    def test_makefastqs_10x_atac_protocol_200(self):
+        """
+        MakeFastqs: '10x_atac' protocol (Cellranger-ATAC 2.0.0)
+        """
+        # Create mock source data
+        illumina_run = MockIlluminaRun(
+            "171020_NB500968_00002_AHGXXXX",
+            "nextseq",
+            bases_mask="y101,I8,I8,y101",
+            top_dir=self.wd)
+        illumina_run.create()
+        run_dir = illumina_run.dirn
+        # Sample sheet with 10xGenomics Chromium SC ATAC-seq indices
+        samplesheet_chromium_sc_atac_indices = """[Header]
+IEMFileVersion,4
+Assay,Nextera XT
+
+[Reads]
+76
+76
+
+[Settings]
+ReverseComplement,0
+Adapter,CTGTCTCTTATACACATCT
+
+[Data]
+Sample_ID,Sample_Name,Sample_Plate,Sample_Well,I7_Index_ID,index,Sample_Project,Description
+smpl1,smpl1,,,A001,SI-NA-A1,10xGenomics,
+smpl2,smpl2,,,A005,SI-NA-B1,10xGenomics,
+"""
+        sample_sheet = os.path.join(self.wd,"SampleSheet.csv")
+        with open(sample_sheet,'w') as fp:
+            fp.write(samplesheet_chromium_sc_atac_indices)
+        # Create mock bcl2fastq and cellranger-atac
+        MockBcl2fastq2Exe.create(os.path.join(self.bin,
+                                              "bcl2fastq"))
+        MockCellrangerExe.create(os.path.join(self.bin,
+                                              "cellranger-atac"),
+                                 version="2.0.0")
+        os.environ['PATH'] = "%s:%s" % (self.bin,
+                                        os.environ['PATH'])
+        analysis_dir = os.path.join(self.wd,"analysis")
+        os.mkdir(analysis_dir)
+        # Do the test
+        p = MakeFastqs(run_dir,sample_sheet,protocol="10x_atac")
+        status = p.run(analysis_dir,
+                       poll_interval=0.5)
+        self.assertEqual(status,0)
+        # Check outputs
+        self.assertEqual(p.output.platform,"nextseq")
+        self.assertEqual(p.output.primary_data_dir,
+                         os.path.join(analysis_dir,
+                                      "primary_data"))
+        self.assertEqual(p.output.bcl2fastq_info,
+                         (os.path.join(self.bin,"bcl2fastq"),
+                          "bcl2fastq",
+                          "2.20.0.422"))
+        self.assertEqual(p.output.cellranger_atac_info,
+                         (os.path.join(self.bin,"cellranger-atac"),
+                          "cellranger-atac",
+                          "2.0.0"))
+        self.assertTrue(p.output.acquired_primary_data)
+        self.assertEqual(p.output.stats_file,
+                         os.path.join(analysis_dir,"statistics.info"))
+        self.assertEqual(p.output.stats_full,
+                         os.path.join(analysis_dir,"statistics_full.info"))
+        self.assertEqual(p.output.per_lane_stats,
+                         os.path.join(analysis_dir,
+                                      "per_lane_statistics.info"))
+        self.assertEqual(p.output.per_lane_sample_stats,
+                         os.path.join(analysis_dir,
+                                      "per_lane_sample_stats.info"))
+        self.assertEqual(p.output.missing_fastqs,[])
+        for subdir in (os.path.join("primary_data",
+                                    "171020_NB500968_00002_AHGXXXX"),
+                       "bcl2fastq",
+                       "barcode_analysis",):
+            self.assertTrue(os.path.isdir(
+                os.path.join(analysis_dir,subdir)),
+                            "Missing subdir: %s" % subdir)
+        self.assertTrue(os.path.islink(
+            os.path.join(analysis_dir,
+                         "primary_data",
+                         "171020_NB500968_00002_AHGXXXX")))
+        for filen in ("statistics.info",
+                      "statistics_full.info",
+                      "per_lane_statistics.info",
+                      "per_lane_sample_stats.info",
+                      "processing_qc.html",):
             self.assertTrue(os.path.isfile(
                 os.path.join(analysis_dir,filen)),
                             "Missing file: %s" % filen)
@@ -3423,7 +3518,7 @@ smpl2,smpl2,,,A005,SI-NA-B1,10xGenomics,
         self.assertEqual(p.output.cellranger_atac_info,
                          (os.path.join(self.bin,"cellranger-atac"),
                           "cellranger-atac",
-                          "1.2.0"))
+                          "2.0.0"))
         self.assertTrue(p.output.acquired_primary_data)
         self.assertEqual(p.output.stats_file,
                          os.path.join(analysis_dir,"statistics.info"))
@@ -3451,8 +3546,7 @@ smpl2,smpl2,,,A005,SI-NA-B1,10xGenomics,
                       "statistics_full.info",
                       "per_lane_statistics.info",
                       "per_lane_sample_stats.info",
-                      "processing_qc.html",
-                      "cellranger-atac_qc_summary.html",):
+                      "processing_qc.html",):
             self.assertTrue(os.path.isfile(
                 os.path.join(analysis_dir,filen)),
                             "Missing file: %s" % filen)
