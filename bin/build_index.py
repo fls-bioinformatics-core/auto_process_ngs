db9447762986b2a209983701eb351f0a2f618d1b
Peter Briggs <peter.briggs@manchester.ac.uk>
2022-05-30 14:10:26 +0100

    build_index.py: get job runner from configuration, or from command line.

--- bin/build_index.py
+++ bin/build_index.py
@@ -9,11 +9,13 @@
 #######################################################################
 
 import argparse
-from bcftbx.JobRunner import SimpleJobRunner
+from bcftbx.JobRunner import fetch_runner
 from auto_process_ngs.indexes import IndexBuilder
+from auto_process_ngs.settings import Settings
 from auto_process_ngs import get_version
 
 __version__ = get_version()
+__settings = Settings()
 
 #######################################################################
 # Main program
@@ -34,10 +36,31 @@ def main():
                    help="annotation file (for use with STAR)")
     p.add_argument('-o',dest='out_dir',action='store',
                    help="output directory for indexes")
+    # Advanced options
+    advanced = p.add_argument_group('Advanced options')
+    advanced.add_argument('-r','--runner',metavar='RUNNER',action='store',
+                          dest="runner",default=None,
+                          help="explicitly specify runner definition for "
+                          "building the index. RUNNER must be a valid job "
+                          "runner specification e.g. "
+                          "'GEJobRunner(-pe smp.pe 8)' (default: use "
+                          "appropriate runner from configuration)")
+    p.add_argument_group()
     args = p.parse_args()
 
+    # Acquire runner
+    if args.runner:
+        # Use runner supplied on command line
+        runner = fetch_runner(args.runner)
+    else:
+        if args.aligner in ("bowtie","bowtie2"):
+            runner = __settings.runners.fastq_screen
+        elif args.aligner == "star":
+            runner = __settings.runners.star
+    print("Job runner: %s" % runner)
+
     # Set up index builder
-    builder = IndexBuilder(SimpleJobRunner(nslots=8))
+    builder = IndexBuilder(runner)
 
     # Build indexes
     if args.aligner == "bowtie":
