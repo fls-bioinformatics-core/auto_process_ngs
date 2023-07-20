282743107a1190ffd8fa96e8feb4ef4e996535c5
Peter Briggs <peter.briggs@manchester.ac.uk>
2023-07-20 16:27:54 +0100

    qc/pipeline: add a unit test for verification detecting 'bad' Fastq file.

--- auto_process_ngs/test/qc/test_pipeline.py
+++ auto_process_ngs/test/qc/test_pipeline.py
@@ -1082,6 +1082,56 @@ PJB1_S1_001.bam	153.754829	69.675347	139	37
                                                         "PJB",f)),
                             "Missing %s" % f)
 
+    def test_qcpipeline_verify_fastqs_with_corrupted_fastq(self):
+        """QCPipeline: verify Fastqs with corrupted file
+        """
+        # Make mock analysis project
+        p = MockAnalysisProject("PJB",("PJB1_S1_R1_001.fastq.gz",
+                                       "PJB1_S1_R2_001.fastq.gz",
+                                       "PJB2_S2_R1_001.fastq.gz",
+                                       "PJB2_S2_R2_001.fastq.gz"),
+                                metadata={ 'Organism': 'Human' })
+        p.create(top_dir=self.wd)
+        # Replace a Fastq file with 'bad' version
+        bad_fastq = os.path.join(self.wd,
+                                 "PJB","fastqs",
+                                 "PJB1_S1_R2_001.fastq.gz")
+        with open(bad_fastq,'wb') as fp:
+            fp.write(b"Corrupted\ndata\nfor\nGzipped file!")
+        # Set up and run the QC
+        runqc = QCPipeline()
+        runqc.add_project(AnalysisProject(os.path.join(self.wd,"PJB")),
+                          fetch_protocol_definition("standardPE"),
+                          multiqc=True)
+        status = runqc.run(fastq_screens=self.fastq_screens,
+                           star_indexes=
+                           { 'human': '/data/hg38/star_index' },
+                           annotation_bed_files=
+                           { 'human': self.ref_data['hg38']['bed'] },
+                           annotation_gtf_files=
+                           { 'human': self.ref_data['hg38']['gtf'] },
+                           poll_interval=POLL_INTERVAL,
+                           max_jobs=1,
+                           runners={ 'default': SimpleJobRunner(), })
+        self.assertEqual(status,1)
+        # Check QC metadata
+        qc_info = AnalysisProjectQCDirInfo(
+            os.path.join(self.wd,"PJB","qc","qc.info"))
+        self.assertEqual(qc_info.protocol,"standardPE")
+        self.assertEqual(qc_info.protocol_specification,
+                         str(fetch_protocol_definition("standardPE")))
+        self.assertEqual(qc_info.organism,"Human")
+        self.assertEqual(qc_info.fastq_dir,
+                         os.path.join(self.wd,"PJB","fastqs"))
+        self.assertEqual(qc_info.fastq_screens,
+                         "model_organisms,other_organisms,rRNA")
+        self.assertEqual(qc_info.star_index,"/data/hg38/star_index")
+        self.assertEqual(qc_info.annotation_bed,self.ref_data['hg38']['bed'])
+        self.assertEqual(qc_info.annotation_gtf,self.ref_data['hg38']['gtf'])
+        self.assertEqual(qc_info.cellranger_version,None)
+        self.assertEqual(qc_info.cellranger_refdata,None)
+        self.assertEqual(qc_info.cellranger_probeset,None)
+
     def test_qcpipeline_single_end(self):
         """QCPipeline: standard QC run (single-end data)
         """
