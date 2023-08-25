928efceab790b26cc139bf642ea18743ed4dab8a
Peter Briggs <peter.briggs@manchester.ac.uk>
2023-08-25 14:56:15 +0100

    Config: cosmetic updates to unit test titles.

--- auto_process_ngs/test/test_config.py
+++ auto_process_ngs/test/test_config.py
@@ -38,12 +38,14 @@ class TestConfig(unittest.TestCase):
         self.config.read_file(fp)
 
     def test_get(self):
-        """Check Config.get fetches correct value
+        """
+        Config: 'get' fetches correct value
         """
         self.assertEqual(self.config.get('basic','greeting'),'hello')
 
     def test_get_with_default(self):
-        """Check Config.get works with defaults
+        """
+        Config: 'get' works with defaults
         """
         self.assertEqual(self.config.get('basic','salutation',
                                          default='bonjour'),
@@ -51,67 +53,74 @@ class TestConfig(unittest.TestCase):
 
     def test_get_with_null(self):
         """
-        Check Config.get returns NullValue for missing section/option
+        Config: 'get' returns NullValue for missing section/option
         """
         self.assertEqual(self.config.get('missing','value'),NullValue())
         self.assertEqual(self.config.get('basic','salutation'),NullValue())
 
     def test_getint(self):
-        """Check Config.getint fetches integer value
+        """
+        Config: 'getint' fetches integer value
         """
         self.assertEqual(self.config.getint('advanced','n'),12)
 
     def test_getint_with_default(self):
-        """Check Config.getint works with defaults
+        """
+        Config: 'getint' works with defaults
         """
         self.assertEqual(self.config.getint('advanced','p',default=11),
                          11)
 
     def test_getint_with_null(self):
         """
-        Check Config.getint returns NullValue for missing section/option
+        Config: 'getint' returns NullValue for missing section/option
         """
         self.assertEqual(self.config.getint('missing','value'),NullValue())
         self.assertEqual(self.config.getint('advanced','p'),NullValue())
 
     def test_getfloat(self):
-        """Check Config.getfloat fetches float value
+        """
+        Config: 'getfloat' fetches float value
         """
         self.assertEqual(self.config.getfloat('advanced','m'),42.0)
 
     def test_getfloat_with_default(self):
-        """Check Config.getfloat works with defaults
+        """
+        Config: 'getfloat' works with defaults
         """
         self.assertEqual(self.config.getfloat('advanced','p',default=5.0),
                          5.0)
 
     def test_getfloat_with_null(self):
         """
-        Check Config.getfloat returns NullValue for missing section/option
+        Config: 'getfloat' returns NullValue for missing section/option
         """
         self.assertEqual(self.config.getint('missing','value'),NullValue())
         self.assertEqual(self.config.getfloat('advanced','p'),NullValue())
 
     def test_getboolean(self):
-        """Check Config.getboolean fetches boolean value
+        """
+        Config: 'getboolean' fetches boolean value
         """
         self.assertEqual(self.config.getboolean('advanced','bool'),True)
 
     def test_getboolean_with_default(self):
-        """Check Config.getboolean works with defaults
+        """
+        Config: 'getboolean' works with defaults
         """
         self.assertEqual(self.config.getboolean('advanced','p',default=True),
                          True)
 
     def test_getboolean_with_null(self):
         """
-        Check Config.getboolean returns NullValue for missing section/option
+        Config: 'getboolean' returns NullValue for missing section/option
         """
         self.assertEqual(self.config.getboolean('missing','value'),NullValue())
         self.assertEqual(self.config.getboolean('advanced','p'),NullValue())
 
     def test_getrunner(self):
-        """Check Config.getrunner fetches correct value
+        """
+        Config: 'getrunner' fetches correct value
         """
         self.assertTrue(
             isinstance(self.config.getrunner('runners',
@@ -119,7 +128,8 @@ class TestConfig(unittest.TestCase):
                        SimpleJobRunner))
 
     def test_getrunner_with_default(self):
-        """Check Config.getrunner works with defaults
+        """
+        Config: 'getrunner' works with defaults
         """
         self.assertTrue(
             isinstance(self.config.getrunner('runners',
@@ -128,7 +138,8 @@ class TestConfig(unittest.TestCase):
                        GEJobRunner))
 
     def test_getrunner_with_runner_instance_as_default(self):
-        """Check Config.getrunner works with runner instance as default
+        """
+        Config: 'getrunner' works with runner instance as default
         """
         self.assertTrue(
             isinstance(self.config.getrunner('runners',
@@ -138,7 +149,7 @@ class TestConfig(unittest.TestCase):
 
     def test_getrunner_with_null(self):
         """
-        Check Config.getrunner returns NullValue for missing section/option
+        Config: 'getrunner' returns NullValue for missing section/option
         """
         self.assertEqual(self.config.getrunner('missing','value'),
                          NullValue())
@@ -146,7 +157,8 @@ class TestConfig(unittest.TestCase):
                          NullValue())
 
     def test_get_with_None_value(self):
-        """Check Config.get works for parameters set to 'None'
+        """
+        Config: 'get' works for parameters set to None
         """
         self.assertEqual(self.config.get('none_types','some_value'),None)
         self.assertEqual(self.config.get('none_types','some_value',
@@ -154,7 +166,8 @@ class TestConfig(unittest.TestCase):
                          'something')
 
     def test_get_with_empty_value(self):
-        """Check Config.get works for parameters with no value set
+        """
+        Config: 'get' works for parameters with no value set
         """
         self.assertEqual(self.config.get('none_types','other_value'),None)
         self.assertEqual(self.config.get('none_types','other_value',
@@ -162,7 +175,8 @@ class TestConfig(unittest.TestCase):
                          'something')
 
     def test_option_case_is_preserved(self):
-        """Check Config.get preserves option case
+        """
+        Config: 'get' preserves option case
         """
         self.assertEqual(self.config.get('names','Name1'),'Peter')
         self.assertEqual(self.config.get('names','name1'),'John')
