9b4dfb250ca9f4114d6ff5427379e7e21415e65d
Peter Briggs <peter.briggs@manchester.ac.uk>
2023-10-24 11:36:32 +0100

    auto_processor: drop functionality from 'update_metadata' now duplicated in 'update' command.

--- auto_process_ngs/auto_processor.py
+++ auto_process_ngs/auto_processor.py
@@ -36,8 +36,6 @@ from .decorators import add_command
 from .metadata import AnalysisDirParameters
 from .metadata import AnalysisDirMetadata
 from .metadata import ProjectMetadataFile
-from .metadata import AnalysisProjectInfo
-from .metadata import AnalysisProjectQCDirInfo
 from .utils import edit_file
 from .utils import get_numbered_subdir
 from .utils import sort_sample_names
@@ -246,7 +244,6 @@ class AutoProcess:
         from other files (for older runs); setting the run name
         """
         # Migrate missing values from parameter file
-        metadata_updated = False
         if self.has_parameter_file:
             # Migrate relevant values across
             print("Migrating metadata values from parameter file")
@@ -315,93 +312,6 @@ class AutoProcess:
                 except KeyError:
                     print("Unable to get sequencer model for "
                           "instrument '%s'" % instrument_name)
-        # Save updated metadata
-        if metadata_updated:
-            print("Saving updated metadata")
-            self.save_metadata(force=True)
-        # Update paths in the top-level parameter file
-        # (if analysis dir has been moved or copied)
-        if self.params.analysis_dir != self.analysis_dir:
-            print("Updating analysis directory paths in parameter file")
-            old_dir = self.params.analysis_dir
-            for p in ('analysis_dir',
-                      'primary_data_dir',
-                      'sample_sheet'):
-                if not self.params[p]:
-                    continue
-                print("...updating '%s'" % p)
-                self.params[p] = os.path.normpath(
-                    os.path.join(self.analysis_dir,
-                                 os.path.relpath(self.params[p],old_dir)))
-            # Update paths in QC metadata in projects
-            for project in self.get_analysis_projects_from_dirs():
-                # Iterate through all project directories
-                for qc_dir in project.qc_dirs:
-                    qc_info = AnalysisProjectQCDirInfo(
-                        os.path.join(project.dirn,qc_dir,"qc.info"))
-                    print("...updating QC info for %s/%s" % (project.name,
-                                                             qc_dir))
-                    qc_info['fastq_dir'] = os.path.normpath(
-                        os.path.join(self.analysis_dir,
-                                     os.path.relpath(qc_info.fastq_dir,
-                                                     old_dir)))
-                    qc_info.save()
-            # Save the updated parameter data
-            self.save_parameters(force=True)
-        # Update project metadata
-        project_metadata = self.load_project_metadata(
-            self.params.project_metadata)
-        save_required = False
-        for line in project_metadata:
-            # Iterate through the named projects
-            name = line['Project']
-            if name.startswith('#'):
-                # Commented out, ignore
-                continue
-            # Look for a matching project directory
-            project_dir = os.path.join(self.analysis_dir,name)
-            if os.path.exists(project_dir):
-                project = AnalysisProject(project_dir)
-                print("Checking metadata for project '%s'" % project.name)
-                # Synchronise metadata in projects with projects.info
-                metadata_items = dict(
-                    user=line['User'],
-                    PI=line['PI'],
-                    organism=line['Organism'],
-                    library_type=line['Library'],
-                    single_cell_platform=line['SC_Platform'],
-                    comments=line['Comments'],
-                    samples=project.sample_summary()
-                )
-                # Only update items where values differ
-                project_metadata_updated = False
-                for item in metadata_items:
-                    new_value = (metadata_items[item]
-                                 if metadata_items[item] != '.' else None)
-                    if project.info[item] != new_value:
-                        print("...updating '%s' => %r" % (item,new_value))
-                        project.info[item] = new_value
-                        project_metadata_updated = True
-                # Check paired-end info
-                project_info = AnalysisProjectInfo(project.info_file)
-                if project_info.paired_end != project.info.paired_end:
-                    print("...updating paired-end info")
-                    project_metadata_updated = True
-                # Save the updated project metadata if required
-                if project_metadata_updated:
-                    print("...saving project metadata")
-                    project.info.save()
-                # Update list of sample names in projects.info
-                sample_list = ','.join(sort_sample_names(
-                    [s.name for s in project.samples]))
-                if line['Samples'] != sample_list:
-                    print("...updating sample list in projects.info")
-                    line['Samples'] = sample_list
-                    save_required = True
-        # Save master project metadata
-        if save_required:
-            print("Saving projects.info")
-            project_metadata.save()
 
     def init_readme(self):
         """
