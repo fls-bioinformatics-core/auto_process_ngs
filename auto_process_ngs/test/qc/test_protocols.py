7344fc1ef3632f8f87b227459931d00531ae4cd4
Peter Briggs <peter.briggs@manchester.ac.uk>
2025-03-20 12:31:28 +0000

    qc/protocols: 'determine_qc_protocol' returns 'standard*' for unknown single cell platform.

--- auto_process_ngs/test/qc/test_protocols.py
+++ auto_process_ngs/test/qc/test_protocols.py
@@ -1354,6 +1354,24 @@ class TestDetermineQCProtocolFunction(unittest.TestCase):
         self.assertEqual(determine_qc_protocol(project),
                          "ParseEvercode")
 
+    def test_determine_qc_protocol_unknown_single_cell_platform(self):
+        """determine_qc_protocol: unknown single cell platform
+        """
+        # Make mock analysis project
+        p = MockAnalysisProject("PJB",("PJB1_S1_R1_001.fastq.gz",
+                                       "PJB1_S1_R2_001.fastq.gz",
+                                       "PJB2_S2_R1_001.fastq.gz",
+                                       "PJB2_S2_R2_001.fastq.gz"),
+                                metadata={'Single cell platform':
+                                          "NewSCPlatform",
+                                          'Library type':
+                                          "scRNA-seq"})
+        p.create(top_dir=self.wd)
+        project = AnalysisProject("PJB",
+                                  os.path.join(self.wd,"PJB"))
+        self.assertEqual(determine_qc_protocol(project),
+                         "standardPE")
+
 class TestFetchProtocolDefinition(unittest.TestCase):
 
     def test_fetch_protocol_definition_from_specification(self):
