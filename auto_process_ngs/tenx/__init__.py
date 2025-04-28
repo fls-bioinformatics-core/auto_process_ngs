68a146baefdacc6755efc8aeb9669579e34cbafa
Peter Briggs <peter.briggs@manchester.ac.uk>
2025-04-28 17:04:49 +0100

    tenx: add "10xGenomics Chromium Next GEM" as recognised 10x platform.

--- auto_process_ngs/tenx/__init__.py
+++ auto_process_ngs/tenx/__init__.py
@@ -11,6 +11,7 @@ PLATFORMS = (
     "10xGenomics Chromium 3'v3.1",
     "10xGenomics Chromium 5'",
     "10xGenomics Chromium GEM-X 3'v4",
+    "10xGenomics Chromium Next GEM",
     "10xGenomics Chromium Next GEM 3'v3.1",
     "10xGenomics Single Cell ATAC",
     "10xGenomics Visium",
