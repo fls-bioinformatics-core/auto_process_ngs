b1414e66a4a0cf5f4ac219aa4611557b26f688f7
Peter Briggs <peter.briggs@manchester.ac.uk>
2025-05-23 18:38:28 +0100

    fetch_data.py: initial WIP implementation of a data fetching utility.

---
+++ auto_process_ngs/cli/fetch_data.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+#
+#     cli/fetch_data.py: utility for fetching data files
+#     Copyright (C) University of Manchester 2025 Peter Briggs
+#
+import os
+import shutil
+import argparse
+import tempfile
+from .. import applications
+from .. import fileops
+from .. import get_version
+from ..settings import Settings
+from ..utils import Location
+import bcftbx.utils as bcf_utils
+
+# Logging
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+def copy_dir_contents(src, dst, replace_spaces=True):
+    """
+    Copy the contents of one directory into another
+
+    Arguments:
+      src (str): path of source directory
+      dst (str): path of destination directory
+      replace_spaces (bool): if True (default) then
+        replace spaces in source names with
+        underscores in the destination names
+    """
+    for f in bcf_utils.walk(src):
+        if f == src:
+            # Don't try to copy the top-level dir
+            continue
+        # Make destination name
+        ff = os.path.relpath(f, src)
+        if replace_spaces:
+            ff = ff.replace(" ","_")
+        ff = os.path.join(dst, ff)
+        if os.path.exists(ff):
+            # Skip existing files
+            logger.warning(f"{ff}: file with this name already exists, "
+                           f"skipping")
+        else:
+            print(f"{os.path.relpath(ff, dst)}")
+            if os.path.isdir(f):
+                os.mkdir(ff)
+            else:
+                # Copy the file directly
+                shutil.copyfile(f, ff, follow_symlinks=False)
+
+# Main function
+
+def main():
+    """
+    """
+    # Load configuration
+    settings = Settings()
+
+    # Collect defaults
+    default_runner = settings.runners.rsync
+
+    # Command line
+    p = argparse.ArgumentParser(
+        description="Copy files and directories from arbitrary "
+        "locations to the local system")
+    p.add_argument('--version', action='version',
+                   version=("%%(prog)s %s" % get_version()))
+    # FIXME add support for flattening directory structure
+    ##p.add_argument('--flatten', action='store_true',
+    ##               help="copy files without replicating the source "
+    ##               "directory structure")
+    # FIXME add support for overwriting files at the destination
+    ##p.add_argument('--overwrite', action='store_true',
+    ##               help="overwrite existing files (default is to skip "
+    ##               "existing files)")
+    # FIXME add support for running rsync via a job runner
+    ##p.add_argument('--runner',action='store',
+    ##               help="specify the job runner to use for executing "
+    ##               "'rsync' operations (defaults to job runner defined "
+    ##               "for copying in config file [%s])" % default_runner)
+    p.add_argument("src", metavar="SOURCE",
+                   help="source data (file or directory) to copy; can "
+                   "be on a local or remote file system")
+    p.add_argument("dst", metavar="DEST",
+                   help="destination on local file system")
+    args = p.parse_args()
+
+    # Source
+    src = args.src
+    remote_src = Location(src).is_remote
+    src_is_dir = fileops.isdir(src)
+    print(f"Source     : {src}")
+
+    # Destination
+    dst = os.path.abspath(args.dst)
+    print(f"Destination: {dst}")
+    dst_exists = os.path.exists(dst)
+
+    # Check source and destination compatibility
+    if dst_exists:
+        if src_is_dir and not os.path.isdir(dst):
+            logger.error(f"Source is a directory but destination is "
+                         f"not")
+            return 1
+
+    # Fetch the data
+    if src_is_dir:
+        # Directory copy
+        # Rsync contents to a temporary dir
+        # FIXME probably only want to do this for remote
+        # FIXME sources in future
+        with tempfile.TemporaryDirectory() as d:
+            # Do rsync
+            print(f"Temporary directory: {d}")
+            if src.endswith("/"):
+                rsync_src = src
+            else:
+                rsync_src = src + "/"
+            rsync = applications.general.rsync(rsync_src, d,
+                                               escape_spaces=False)
+            print(f"Running {rsync.command_line}")
+            # FIXME run rsync locally for now
+            # FIXME but should use a runner
+            retcode, output = rsync.subprocess_check_output()
+            if retcode != 0:
+                # Rsync didn't complete successfully
+                logger.error(f"Error running rsync: {output}")
+                return 1
+            else:
+                print(f"Rsync ok: {output}")
+            # Make final directory if it doesn't exist
+            if not os.path.exists(dst):
+                os.mkdir(dst)
+                print(f"Made destination directory '{dst}'") 
+            # Copy the contents to final location
+            print(f"Copying into '{dst}'")
+            copy_dir_contents(d, dst)
+    else:
+        # File copy
+        # Rsync file to a temporary dir
+        with tempfile.TemporaryDirectory() as d:
+            print(f"Temporary directory: {d}")
+            # Do rsync
+            rsync = applications.general.rsync(src, d,
+                                               escape_spaces=False)
+            print(f"Running {rsync.command_line}")
+            retcode, output = rsync.subprocess_check_output()
+            if retcode != 0:
+                # Rsync didn't complete successfully
+                logger.error(f"Error running rsync: {output}")
+                return 1
+            else:
+                print(f"Rsync ok: {output}")
+            # Copy file to final location
+            f = os.path.basename(Location(src).path)
+            if os.path.isdir(dst):
+                print(f"{f}")
+                shutil.copyfile(os.path.join(d, f),
+                                os.path.join(dst, f))
+            else:
+                print(f"{os.path.basename(dst)}")
+                shutil.copyfile(os.path.join(d, f), dst)
+
