0a000cfe64d7c3279602b81fa60c32a23df0ed4f
Peter Briggs <peter.briggs@manchester.ac.uk>
2025-10-03 12:46:41 +0100

    bcl2fastq/pipeline: '10x_atac' sets R1, R2 and R3 lengths automatically.

--- auto_process_ngs/test/bcl2fastq/pipeline/test_10x_atac.py
+++ auto_process_ngs/test/bcl2fastq/pipeline/test_10x_atac.py
@@ -18,7 +18,7 @@ class TestMakeFastqs(BaseMakeFastqsTestCase):
         illumina_run = MockIlluminaRun(
             "171020_NB500968_00002_AHGXXXX",
             "nextseq",
-            bases_mask="y101,I8,I8,y101",
+            bases_mask="y101,I8,I20,y101",
             top_dir=self.wd)
         illumina_run.create()
         run_dir = illumina_run.dirn
@@ -49,7 +49,7 @@ smpl2,smpl2,,,A005,SI-NA-B1,A005,SI-NA-B1,10xGenomics,
         MockCellrangerExe.create(os.path.join(self.bin,
                                               "cellranger-atac"),
                                  version="1.2.0",
-                                 assert_bases_mask="Y101,I8,Y8,Y101")
+                                 assert_bases_mask="Y50N51,I8,Y16N4,Y50N51")
         os.environ['PATH'] = "%s:%s" % (self.bin,
                                         os.environ['PATH'])
         analysis_dir = os.path.join(self.wd,"analysis")
@@ -119,7 +119,7 @@ smpl2,smpl2,,,A005,SI-NA-B1,A005,SI-NA-B1,10xGenomics,
         illumina_run = MockIlluminaRun(
             "171020_NB500968_00002_AHGXXXX",
             "nextseq",
-            bases_mask="y101,I8,I8,y101",
+            bases_mask="y101,I8,I20,y101",
             top_dir=self.wd)
         illumina_run.create()
         run_dir = illumina_run.dirn
@@ -150,13 +150,116 @@ smpl2,smpl2,,,A005,SI-NA-B1,A005,SI-NA-B1,10xGenomics,
         MockCellrangerExe.create(os.path.join(self.bin,
                                               "cellranger-atac"),
                                  version="2.0.0",
-                                 assert_bases_mask="Y101,I8,Y8,Y101")
+                                 assert_bases_mask="Y50N51,I8,Y16N4,Y50N51")
         os.environ['PATH'] = "%s:%s" % (self.bin,
                                         os.environ['PATH'])
         analysis_dir = os.path.join(self.wd,"analysis")
         os.mkdir(analysis_dir)
         # Do the test
         p = MakeFastqs(run_dir,sample_sheet,protocol="10x_atac")
+        status = p.run(analysis_dir,
+                       poll_interval=POLL_INTERVAL)
+        self.assertEqual(status,0)
+        # Check outputs
+        self.assertEqual(p.output.platform,"nextseq")
+        self.assertEqual(p.output.flow_cell_mode,None)
+        self.assertEqual(p.output.primary_data_dir,
+                         os.path.join(analysis_dir,
+                                      "primary_data"))
+        self.assertEqual(p.output.bcl2fastq_info,
+                         (os.path.join(self.bin,"bcl2fastq"),
+                          "bcl2fastq",
+                          "2.20.0.422"))
+        self.assertEqual(p.output.cellranger_atac_info,
+                         (os.path.join(self.bin,"cellranger-atac"),
+                          "cellranger-atac",
+                          "2.0.0"))
+        self.assertTrue(p.output.acquired_primary_data)
+        self.assertEqual(p.output.stats_file,
+                         os.path.join(analysis_dir,"statistics.info"))
+        self.assertEqual(p.output.stats_full,
+                         os.path.join(analysis_dir,"statistics_full.info"))
+        self.assertEqual(p.output.per_lane_stats,
+                         os.path.join(analysis_dir,
+                                      "per_lane_statistics.info"))
+        self.assertEqual(p.output.per_lane_sample_stats,
+                         os.path.join(analysis_dir,
+                                      "per_lane_sample_stats.info"))
+        self.assertEqual(p.output.seq_len_stats,
+                         os.path.join(analysis_dir,
+                                      "seq_len_statistics.info"))
+        self.assertEqual(p.output.missing_fastqs,[])
+        for subdir in (os.path.join("primary_data",
+                                    "171020_NB500968_00002_AHGXXXX"),
+                       "bcl2fastq",
+                       "barcode_analysis",):
+            self.assertTrue(os.path.isdir(
+                os.path.join(analysis_dir,subdir)),
+                            "Missing subdir: %s" % subdir)
+        self.assertTrue(os.path.islink(
+            os.path.join(analysis_dir,
+                         "primary_data",
+                         "171020_NB500968_00002_AHGXXXX")))
+        for filen in ("statistics.info",
+                      "statistics_full.info",
+                      "per_lane_statistics.info",
+                      "per_lane_sample_stats.info",
+                      "seq_len_statistics.info",
+                      "processing_qc.html",):
+            self.assertTrue(os.path.isfile(
+                os.path.join(analysis_dir,filen)),
+                            "Missing file: %s" % filen)
+
+    #@unittest.skip("Skipped")
+    def test_makefastqs_10x_atac_protocol_truncate_reads(self):
+        """
+        MakeFastqs: '10x_atac' protocol (truncate reads)
+        """
+        # Create mock source data
+        illumina_run = MockIlluminaRun(
+            "171020_NB500968_00002_AHGXXXX",
+            "nextseq",
+            bases_mask="y101,I8,I20,y101",
+            top_dir=self.wd)
+        illumina_run.create()
+        run_dir = illumina_run.dirn
+        # Sample sheet with 10xGenomics Chromium SC ATAC-seq indices
+        samplesheet_chromium_sc_atac_indices = """[Header]
+IEMFileVersion,4
+Assay,Nextera XT
+
+[Reads]
+76
+76
+
+[Settings]
+ReverseComplement,0
+Adapter,CTGTCTCTTATACACATCT
+
+[Data]
+Sample_ID,Sample_Name,Sample_Plate,Sample_Well,I7_Index_ID,index,I5_Index_ID,index2,Sample_Project,Description
+smpl1,smpl1,,,A001,SI-NA-A1,A001,SI-NA-A1,10xGenomics,
+smpl2,smpl2,,,A005,SI-NA-B1,A005,SI-NA-B1,10xGenomics,
+"""
+        sample_sheet = os.path.join(self.wd,"SampleSheet.csv")
+        with open(sample_sheet,'w') as fp:
+            fp.write(samplesheet_chromium_sc_atac_indices)
+        # Create mock bcl2fastq and cellranger-atac
+        MockBcl2fastq2Exe.create(os.path.join(self.bin,
+                                              "bcl2fastq"))
+        MockCellrangerExe.create(os.path.join(self.bin,
+                                              "cellranger-atac"),
+                                 version="2.0.0",
+                                 assert_bases_mask="Y75N26,I8,Y18N2,Y75N26")
+        os.environ['PATH'] = "%s:%s" % (self.bin,
+                                        os.environ['PATH'])
+        analysis_dir = os.path.join(self.wd,"analysis")
+        os.mkdir(analysis_dir)
+        # Do the test
+        p = MakeFastqs(run_dir,sample_sheet,protocol="10x_atac",
+                       r1_length=75,
+                       r2_length=18,
+                       r3_length=75)
         status = p.run(analysis_dir,
                        poll_interval=POLL_INTERVAL)
         self.assertEqual(status,0)
