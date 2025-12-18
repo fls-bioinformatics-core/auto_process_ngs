4c0dcbefbf4a624723a0739fb955f7b69a49d7ae
Peter Briggs <peter.briggs@manchester.ac.uk>
2025-12-18 11:18:38 +0000

    qc/modules/cellranger_count: add tests for CellRanger version 10.

--- auto_process_ngs/test/qc/modules/test_cellranger_count.py
+++ auto_process_ngs/test/qc/modules/test_cellranger_count.py
@@ -545,7 +545,7 @@ class TestQCPipelineCellrangerCount(BaseQCPipelineTestCase):
         # Check number of cells
         project_metadata = AnalysisProjectInfo(
             os.path.join(self.wd,"PJB","README.info"))
-        self.assertEqual(project_metadata.number_of_cells,11058)
+        self.assertEqual(project_metadata.number_of_cells,29360)
         # Check QC metadata
         qc_info = AnalysisProjectQCDirInfo(
             os.path.join(self.wd,"PJB","qc","qc.info"))
@@ -569,6 +569,97 @@ class TestQCPipelineCellrangerCount(BaseQCPipelineTestCase):
         self.assertEqual(qc_info.cellranger_version,"9.0.0")
         self.assertEqual(qc_info.cellranger_refdata,
                          "/data/refdata-gex-GRCh38-2020-A")
+        self.assertEqual(qc_info.cellranger_probeset,None)
+        # Check reports
+        for f in ("qc_report.html",
+                  "qc_report.PJB.zip"):
+            self.assertTrue(os.path.exists(os.path.join(self.wd,
+                                                        "PJB",f)),
+                            "Missing %s" % f)
+
+    def test_qcpipeline_qc_modules_cellranger_count_scrnaseq_10_0_0(self):
+        """
+        QCPipeline: 'cellranger_count' QC module (scRNA-seq, Cellranger v10.0.0)
+        """
+        # Make mock QC executables
+        MockCellrangerExe.create(os.path.join(self.bin,"cellranger"),
+                                 version="10.0.0",
+                                 assert_include_introns=True)
+        os.environ['PATH'] = "%s:%s" % (self.bin,
+                                        os.environ['PATH'])
+        # Make mock analysis project
+        p = MockAnalysisProject("PJB",("PJB1_S1_R1_001.fastq.gz",
+                                       "PJB1_S1_R2_001.fastq.gz",
+                                       "PJB2_S2_R1_001.fastq.gz",
+                                       "PJB2_S2_R2_001.fastq.gz"),
+                                metadata={ 'Organism': 'Human',
+                                           'Single cell platform':
+                                           '10x Chromium 3\' (v4 GEM-X)',
+                                           'Library type': 'scRNA-seq' })
+        p.create(top_dir=self.wd)
+        # QC protocol
+        protocol = QCProtocol(name="cellranger_count",
+                              description="Cellranger_count test",
+                              seq_data_reads=['r2',],
+                              index_reads=['r1'],
+                              qc_modules=("cellranger_count",))
+        # Set up and run the QC
+        runqc = QCPipeline()
+        runqc.add_project(AnalysisProject(os.path.join(self.wd,"PJB")),
+                          protocol)
+        status = runqc.run(cellranger_transcriptomes=
+                           { 'human':
+                             '/data/refdata-gex-GRCh38-2024-A' },
+                           poll_interval=POLL_INTERVAL,
+                           max_jobs=1,
+                           runners={ 'default': SimpleJobRunner(), })
+        self.assertEqual(status,0)
+        # Check outputs
+        project_dir = os.path.join(self.wd,"PJB")
+        for f in (
+                "qc/cellranger_count",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/_cmdline",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/outs/web_summary.html",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/outs/metrics_summary.csv",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/_cmdline",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/outs/web_summary.html",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/outs/metrics_summary.csv",
+                "cellranger_count",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/_cmdline",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/outs/web_summary.html",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/outs/metrics_summary.csv",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/_cmdline",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/outs/web_summary.html",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/outs/metrics_summary.csv"):
+            self.assertTrue(os.path.exists(os.path.join(project_dir,f)),
+                            "%s: missing" % f)
+        # Check number of cells
+        project_metadata = AnalysisProjectInfo(
+            os.path.join(self.wd,"PJB","README.info"))
+        self.assertEqual(project_metadata.number_of_cells,29360)
+        # Check QC metadata
+        qc_info = AnalysisProjectQCDirInfo(
+            os.path.join(self.wd,"PJB","qc","qc.info"))
+        self.assertEqual(qc_info.protocol,"cellranger_count")
+        self.assertEqual(qc_info.protocol_specification,
+                         str(protocol))
+        self.assertEqual(qc_info.organism,"Human")
+        self.assertEqual(qc_info.seq_data_samples,"PJB1,PJB2")
+        self.assertEqual(qc_info.fastq_dir,
+                         os.path.join(self.wd,"PJB","fastqs"))
+        self.assertEqual(qc_info.fastqs,
+                         "PJB1_S1_R1_001.fastq.gz,"
+                         "PJB1_S1_R2_001.fastq.gz,"
+                         "PJB2_S2_R1_001.fastq.gz,"
+                         "PJB2_S2_R2_001.fastq.gz")
+        self.assertEqual(qc_info.fastqs_split_by_lane,False)
+        self.assertEqual(qc_info.fastq_screens,None)
+        self.assertEqual(qc_info.star_index,None)
+        self.assertEqual(qc_info.annotation_bed,None)
+        self.assertEqual(qc_info.annotation_gtf,None)
+        self.assertEqual(qc_info.cellranger_version,"10.0.0")
+        self.assertEqual(qc_info.cellranger_refdata,
+                         "/data/refdata-gex-GRCh38-2024-A")
         self.assertEqual(qc_info.cellranger_probeset,None)
         # Check reports
         for f in ("qc_report.html",
@@ -1091,7 +1182,7 @@ class TestQCPipelineCellrangerCount(BaseQCPipelineTestCase):
         # Check number of cells
         project_metadata = AnalysisProjectInfo(
             os.path.join(self.wd,"PJB","README.info"))
-        self.assertEqual(project_metadata.number_of_cells,11058)
+        self.assertEqual(project_metadata.number_of_cells,29360)
         # Check QC metadata
         qc_info = AnalysisProjectQCDirInfo(
             os.path.join(self.wd,"PJB","qc","qc.info"))
@@ -1115,6 +1206,97 @@ class TestQCPipelineCellrangerCount(BaseQCPipelineTestCase):
         self.assertEqual(qc_info.cellranger_version,"9.0.0")
         self.assertEqual(qc_info.cellranger_refdata,
                          "/data/refdata-gex-GRCh38-2020-A")
+        self.assertEqual(qc_info.cellranger_probeset,None)
+        # Check reports
+        for f in ("qc_report.html",
+                  "qc_report.PJB.zip"):
+            self.assertTrue(os.path.exists(os.path.join(self.wd,
+                                                        "PJB",f)),
+                            "Missing %s" % f)
+
+    def test_qcpipeline_qc_modules_cellranger_count_snrnaseq_10_0_0(self):
+        """
+        QCPipeline: 'cellranger_count' QC module (snRNA-seq, Cellranger v10.0.0)
+        """
+        # Make mock QC executables
+        MockCellrangerExe.create(os.path.join(self.bin,"cellranger"),
+                                 version="10.0.0",
+                                 assert_include_introns=True)
+        os.environ['PATH'] = "%s:%s" % (self.bin,
+                                        os.environ['PATH'])
+        # Make mock analysis project
+        p = MockAnalysisProject("PJB",("PJB1_S1_R1_001.fastq.gz",
+                                       "PJB1_S1_R2_001.fastq.gz",
+                                       "PJB2_S2_R1_001.fastq.gz",
+                                       "PJB2_S2_R2_001.fastq.gz"),
+                                metadata={ 'Organism': 'Human',
+                                           'Single cell platform':
+                                           '10x Chromium 3\' (v4 GEM-X)',
+                                           'Library type': 'snRNA-seq' })
+        p.create(top_dir=self.wd)
+        # QC protocol
+        protocol = QCProtocol(name="cellranger_count",
+                              description="Cellranger_count test",
+                              seq_data_reads=['r2',],
+                              index_reads=['r1'],
+                              qc_modules=("cellranger_count",))
+        # Set up and run the QC
+        runqc = QCPipeline()
+        runqc.add_project(AnalysisProject(os.path.join(self.wd,"PJB")),
+                          protocol)
+        status = runqc.run(cellranger_transcriptomes=
+                           { 'human':
+                             '/data/refdata-gex-GRCh38-2024-A' },
+                           poll_interval=POLL_INTERVAL,
+                           max_jobs=1,
+                           runners={ 'default': SimpleJobRunner(), })
+        self.assertEqual(status,0)
+        # Check outputs
+        project_dir = os.path.join(self.wd,"PJB")
+        for f in (
+                "qc/cellranger_count",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/_cmdline",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/outs/web_summary.html",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/outs/metrics_summary.csv",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/_cmdline",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/outs/web_summary.html",
+                "qc/cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/outs/metrics_summary.csv",
+                "cellranger_count",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/_cmdline",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/outs/web_summary.html",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB1/outs/metrics_summary.csv",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/_cmdline",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/outs/web_summary.html",
+                "cellranger_count/10.0.0/refdata-gex-GRCh38-2024-A/PJB2/outs/metrics_summary.csv"):
+            self.assertTrue(os.path.exists(os.path.join(project_dir,f)),
+                            "%s: missing" % f)
+        # Check number of cells
+        project_metadata = AnalysisProjectInfo(
+            os.path.join(self.wd,"PJB","README.info"))
+        self.assertEqual(project_metadata.number_of_cells,29360)
+        # Check QC metadata
+        qc_info = AnalysisProjectQCDirInfo(
+            os.path.join(self.wd,"PJB","qc","qc.info"))
+        self.assertEqual(qc_info.protocol,"cellranger_count")
+        self.assertEqual(qc_info.protocol_specification,
+                         str(protocol))
+        self.assertEqual(qc_info.organism,"Human")
+        self.assertEqual(qc_info.seq_data_samples,"PJB1,PJB2")
+        self.assertEqual(qc_info.fastq_dir,
+                         os.path.join(self.wd,"PJB","fastqs"))
+        self.assertEqual(qc_info.fastqs,
+                         "PJB1_S1_R1_001.fastq.gz,"
+                         "PJB1_S1_R2_001.fastq.gz,"
+                         "PJB2_S2_R1_001.fastq.gz,"
+                         "PJB2_S2_R2_001.fastq.gz")
+        self.assertEqual(qc_info.fastqs_split_by_lane,False)
+        self.assertEqual(qc_info.fastq_screens,None)
+        self.assertEqual(qc_info.star_index,None)
+        self.assertEqual(qc_info.annotation_bed,None)
+        self.assertEqual(qc_info.annotation_gtf,None)
+        self.assertEqual(qc_info.cellranger_version,"10.0.0")
+        self.assertEqual(qc_info.cellranger_refdata,
+                         "/data/refdata-gex-GRCh38-2024-A")
         self.assertEqual(qc_info.cellranger_probeset,None)
         # Check reports
         for f in ("qc_report.html",
