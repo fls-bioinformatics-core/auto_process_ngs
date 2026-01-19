a315e53ab7fc5b378273e47598dae7a8f2b5b58b
Peter Briggs <peter.briggs@manchester.ac.uk>
2026-01-19 09:41:36 +0000

    qc/modules/sequence_lengths: add test for '.fq' Fastq extension.
    Add unit test to check that 'sequence_lengths' QC module can handle Fastq files with '.fq' extension.

--- auto_process_ngs/test/qc/modules/test_sequence_lengths.py
+++ auto_process_ngs/test/qc/modules/test_sequence_lengths.py
@@ -331,6 +331,67 @@ class TestQCPipelineSequenceLengths(BaseQCPipelineTestCase):
                                                         "PJB",f)),
                             "Missing %s" % f)
 
+    def test_qcpipeline_qc_modules_sequence_lengths_se_fq_files(self):
+        """
+        QCPipeline: 'sequence_lengths' QC module (SE data, '.fq' Fastq extension)
+        """
+        # Make mock analysis project
+        p = MockAnalysisProject("PJB",("PJB1_S1_R1_001.fq.gz",
+                                       "PJB2_S2_R1_001.fq.gz"),
+                                metadata={ 'Organism': 'Human' })
+        p.create(top_dir=self.wd)
+        # QC protocol
+        protocol = QCProtocol(name="sequence_lengths",
+                              description="Sequence lengths test",
+                              seq_data_reads=['r1',],
+                              index_reads=None,
+                              qc_modules=("sequence_lengths",))
+        # Set up and run the QC
+        runqc = QCPipeline()
+        runqc.add_project(AnalysisProject(os.path.join(self.wd,"PJB")),
+                          protocol)
+        status = runqc.run(poll_interval=POLL_INTERVAL,
+                           max_jobs=1,
+                           runners={ 'default': SimpleJobRunner(), })
+        self.assertEqual(status,0)
+        # Check outputs
+        qc_dir = os.path.join(self.wd,"PJB","qc")
+        for f in ("PJB1_S1_R1_001_seqlens.json",
+                  "PJB2_S2_R1_001_seqlens.json"):
+            self.assertTrue(os.path.exists(os.path.join(qc_dir,f)),
+                            "%s: missing" % f)
+        for f in ("PJB1_S1_I1_001_seqlens.json",
+                  "PJB2_S2_I1_001_seqlens.json"):
+            self.assertFalse(os.path.exists(os.path.join(qc_dir,f)),
+                             "%s: present" % f)
+        # Check QC metadata
+        qc_info = AnalysisProjectQCDirInfo(
+            os.path.join(self.wd,"PJB","qc","qc.info"))
+        self.assertEqual(qc_info.protocol,"sequence_lengths")
+        self.assertEqual(qc_info.protocol_specification,
+                         str(protocol))
+        self.assertEqual(qc_info.organism,"Human")
+        self.assertEqual(qc_info.seq_data_samples,"PJB1,PJB2")
+        self.assertEqual(qc_info.fastq_dir,
+                         os.path.join(self.wd,"PJB","fastqs"))
+        self.assertEqual(qc_info.fastqs,
+                         "PJB1_S1_R1_001.fq.gz,"
+                         "PJB2_S2_R1_001.fq.gz")
+        self.assertEqual(qc_info.fastqs_split_by_lane,False)
+        self.assertEqual(qc_info.fastq_screens,None)
+        self.assertEqual(qc_info.star_index,None)
+        self.assertEqual(qc_info.annotation_bed,None)
+        self.assertEqual(qc_info.annotation_gtf,None)
+        self.assertEqual(qc_info.cellranger_version,None)
+        self.assertEqual(qc_info.cellranger_refdata,None)
+        self.assertEqual(qc_info.cellranger_probeset,None)
+        # Check reports
+        for f in ("qc_report.html",
+                  "qc_report.PJB.zip"):
+            self.assertTrue(os.path.exists(os.path.join(self.wd,
+                                                        "PJB",f)),
+                            "Missing %s" % f)
+
     def test_qcpipeline_qc_modules_sequence_lengths_se_split_lanes(self):
         """
         QCPipeline: 'sequence_lengths' QC module (SE data, split by lane)
